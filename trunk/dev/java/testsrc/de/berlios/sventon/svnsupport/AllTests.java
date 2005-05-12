6e57f02c91d17bb1eb4be469b6c4e9bb27ac3fb6
patrikfr <patrikfr@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2005-05-12 22:28:32 +0000

    Added comparator for SVNDirEntry with tests.

---
+++ trunk/dev/java/testsrc/de/berlios/sventon/svnsupport/AllTests.java
@@ -0,0 +1,20 @@
+package de.berlios.sventon.svnsupport;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(AllTests.suite());
+  }
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite("Test for de.berlios.sventon.svnsupport");
+    //$JUnit-BEGIN$
+    suite.addTestSuite(SVNDirEntryComparatorTest.class);
+    //$JUnit-END$
+    return suite;
+  }
+
+}
