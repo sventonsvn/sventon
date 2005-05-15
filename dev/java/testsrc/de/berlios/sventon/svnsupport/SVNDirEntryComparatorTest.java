b5cc4ed5ed1ebc231b42b3ba39af90084c01fb4c
Patrik Fredriksson <patrikfr@users.noreply.github.com>
2005-05-15 20:28:10 +0000

    Adde sorting to browser view. Updated Javadoc. Removed hard coded path in log4j.properties.

--- dev/java/testsrc/de/berlios/sventon/svnsupport/SVNDirEntryComparatorTest.java
+++ dev/java/testsrc/de/berlios/sventon/svnsupport/SVNDirEntryComparatorTest.java
@@ -8,9 +8,15 @@ import java.util.List;
 import junit.framework.TestCase;
 
 import org.tmatesoft.svn.core.io.SVNDirEntry;
-import org.tmatesoft.svn.core.io.SVNNodeKind;
+import static org.tmatesoft.svn.core.io.SVNNodeKind.FILE;
+import static org.tmatesoft.svn.core.io.SVNNodeKind.DIR;
 
-import de.berlios.sventon.svnsupport.SVNDirEntryComparator;
+import static de.berlios.sventon.svnsupport.SVNDirEntryComparator.AUTHOR;
+import static de.berlios.sventon.svnsupport.SVNDirEntryComparator.DATE;
+import static de.berlios.sventon.svnsupport.SVNDirEntryComparator.NAME;
+import static de.berlios.sventon.svnsupport.SVNDirEntryComparator.REVISION;
+
+;
 
 public class SVNDirEntryComparatorTest extends TestCase {
 
@@ -32,49 +38,49 @@ public class SVNDirEntryComparatorTest extends TestCase {
 
   public final void testCompare() {
     List<SVNDirEntry> entries = new ArrayList<SVNDirEntry>();
-    SVNDirEntry e1 = new SVNDirEntry("FirstClass.java", SVNNodeKind.FILE, 134, false, 2, new GregorianCalendar(2005, 4,
-        12).getTime(), "patrikfr");
-    SVNDirEntry e2 = new SVNDirEntry("SecondClass.java", SVNNodeKind.FILE, 135, false, 3, new GregorianCalendar(2005,
-        4, 13).getTime(), "jesper");
-    SVNDirEntry e3 = new SVNDirEntry("ThirdClass.java", SVNNodeKind.DIR, 136, false, 4, new GregorianCalendar(2005, 4,
-        14).getTime(), "patrikfr");
+    SVNDirEntry e1 = new SVNDirEntry("FirstClass.java", FILE, 134, false, 2, new GregorianCalendar(2005, 4, 12)
+        .getTime(), "patrikfr");
+    SVNDirEntry e2 = new SVNDirEntry("SecondClass.java", FILE, 135, false, 3, new GregorianCalendar(2005, 4, 13)
+        .getTime(), "jesper");
+    SVNDirEntry e3 = new SVNDirEntry("ThirdClass.java", DIR, 136, false, 4, new GregorianCalendar(2005, 4, 14)
+        .getTime(), "patrikfr");
     entries.add(e3);
     entries.add(e2);
     entries.add(e1);
 
     assertSame(e3, entries.get(0));
 
-    Collections.sort(entries, new SVNDirEntryComparator(SVNDirEntryComparator.NAME, false));
+    Collections.sort(entries, new SVNDirEntryComparator(NAME, false));
     assertSame(e1, entries.get(0));
     assertSame(e2, entries.get(1));
     assertSame(e3, entries.get(2));
 
-    Collections.sort(entries, new SVNDirEntryComparator(SVNDirEntryComparator.NAME, true));
+    Collections.sort(entries, new SVNDirEntryComparator(NAME, true));
     assertSame(e1, entries.get(1));
     assertSame(e2, entries.get(2));
     assertSame(e3, entries.get(0));
 
-    Collections.sort(entries, new SVNDirEntryComparator(SVNDirEntryComparator.AUTHOR, false));
+    Collections.sort(entries, new SVNDirEntryComparator(AUTHOR, false));
     assertSame(e1, entries.get(1));
     assertSame(e2, entries.get(0));
     assertSame(e3, entries.get(2));
 
-    Collections.sort(entries, new SVNDirEntryComparator(SVNDirEntryComparator.AUTHOR, true));
+    Collections.sort(entries, new SVNDirEntryComparator(AUTHOR, true));
     assertSame(e1, entries.get(2));
     assertSame(e2, entries.get(1));
     assertSame(e3, entries.get(0));
 
-    Collections.sort(entries, new SVNDirEntryComparator(SVNDirEntryComparator.REVISION, false));
+    Collections.sort(entries, new SVNDirEntryComparator(REVISION, false));
     assertSame(e1, entries.get(0));
     assertSame(e2, entries.get(1));
     assertSame(e3, entries.get(2));
 
-    Collections.sort(entries, new SVNDirEntryComparator(SVNDirEntryComparator.DATE, false));
+    Collections.sort(entries, new SVNDirEntryComparator(DATE, false));
     assertSame(e1, entries.get(0));
     assertSame(e2, entries.get(1));
     assertSame(e3, entries.get(2));
 
-    //Tricking the constructor with an illegal type should fail fast
+    // Tricking the constructor with an illegal type should fail fast
     try {
       new SVNDirEntryComparator(4, false);
       fail("IllegalArgumentException expected");
@@ -82,7 +88,7 @@ public class SVNDirEntryComparatorTest extends TestCase {
       // Expected
     }
 
-    //null values are not OK
+    // null values are not OK
     SVNDirEntryComparator comparator = new SVNDirEntryComparator(SVNDirEntryComparator.DATE, false);
     try {
       comparator.compare(null, null);
@@ -91,6 +97,22 @@ public class SVNDirEntryComparatorTest extends TestCase {
       // Expected
     }
 
+    // Test handling of null properties in SVNDirEntry
+    entries = new ArrayList<SVNDirEntry>();
+    e1 = new SVNDirEntry("FirstClass.java", FILE, 134, false, 2, new GregorianCalendar(2005, 4, 12).getTime(),
+        "patrikfr");
+    e2 = new SVNDirEntry(null, FILE, 135, false, 3, new GregorianCalendar(2005, 4, 13).getTime(),
+        "jesper");
+    e3 = new SVNDirEntry("ThirdClass.java", DIR, 136, false, 4, new GregorianCalendar(2005, 4, 14).getTime(),
+        "patrikfr");
+    entries.add(e3);
+    entries.add(e2);
+    entries.add(e1);
+    
+    Collections.sort(entries, new SVNDirEntryComparator(NAME, false));
+    assertSame(e1, entries.get(1));
+    assertSame(e2, entries.get(0));
+    assertSame(e3, entries.get(2));
   }
 
 }
