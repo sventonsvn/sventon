9a4e1a292c222d9db519dfbfae6f8fbfb04d375c
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2005-09-20 19:05:48 +0000

    Added new web pages.

---
+++ trunk/web/features.php
@@ -0,0 +1,18 @@
+<p>
+<b>Current sventon features:</b>
+</p>
+
+<ul>
+  <li>Easy installation and setup.</li>
+  <li>WebDAV and svnserve repository browsning.</li>
+  <li>View and download files for any given revision.</li>
+  <li>View logs history and metadata for files and directories.</li>
+  <li>Source code syntax highlighting using <a href="https://jhighlight.dev.java.net">JHighlight</a>.</li>
+  <li>Resource based for easy translation.</li>
+  <li>Style sheet marked up for easy customization.</li>
+  <li>Single account authentication support for non public repositories.</li>
+  <li>File and directory name search.</li>
+  <li>Directory flattening for fast navigation.</li>
+  <li>Image thumbnail preview.</li>
+  <li>Keyword expansion when viewing files.</li>
+</ul>
