2ef76332c0a8fc9c7c812fda95446df06dbc6709
Jesper Hammarbäck <jesper@hammarback.eu>
2005-10-30 21:56:05 +0000

    Created test for FlattenControllerTest.

--- dev/java/testsrc/de/berlios/sventon/index/RevisionIndexerTest.java
+++ dev/java/testsrc/de/berlios/sventon/index/RevisionIndexerTest.java
@@ -2,40 +2,14 @@ package de.berlios.sventon.index;
 
 import de.berlios.sventon.svnsupport.SVNRepositoryStub;
 import junit.framework.TestCase;
-import org.tmatesoft.svn.core.SVNDirEntry;
-import org.tmatesoft.svn.core.SVNNodeKind;
-import org.tmatesoft.svn.core.SVNURL;
-
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
-import java.util.List;
 
 public class RevisionIndexerTest extends TestCase {
 
   private RevisionIndexer indexer = null;
 
   public void setUp() throws Exception {
-    // Set up the repository stub
-    SVNRepositoryStub repository = new SVNRepositoryStub(SVNURL.parseURIDecoded("http://localhost"), null);
-    repository.setLatestRevision(123);
-
-    List<SVNDirEntry> entries1 = new ArrayList<SVNDirEntry>();
-    entries1.add(new SVNDirEntry("file1.java", SVNNodeKind.FILE, 64000, false, 1, new Date(), "jesper"));
-    entries1.add(new SVNDirEntry("file2.html", SVNNodeKind.FILE, 32000, false, 2, new Date(), "jesper"));
-    entries1.add(new SVNDirEntry("dir1", SVNNodeKind.DIR, 0, false, 1, new Date(), "jesper"));
-    entries1.add(new SVNDirEntry("file3.java", SVNNodeKind.FILE, 16000, false, 3, new Date(), "jesper"));
-    List<SVNDirEntry> entries2 = new ArrayList<SVNDirEntry>();
-    entries2.add(new SVNDirEntry("dir2", SVNNodeKind.DIR, 0, false, 1, new Date(), "jesper"));
-    entries2.add(new SVNDirEntry("dirfile1.java", SVNNodeKind.FILE, 6400, false, 1, new Date(), "jesper"));
-    entries2.add(new SVNDirEntry("dirfile2.html", SVNNodeKind.FILE, 3200, false, 2, new Date(), "jesper"));
-    entries2.add(new SVNDirEntry("dirfile3.java", SVNNodeKind.FILE, 1600, false, 3, new Date(), "jesper"));
-
-    repository.addDir("/", entries1);
-    repository.addDir("/dir1/", entries2);
-    repository.addDir("/dir1/dir2/", new ArrayList());
-
-    indexer = new RevisionIndexer(repository);
+    indexer = new RevisionIndexer(SVNRepositoryStub.getInstance());
     indexer.index();
     assertEquals(8, indexer.getIndexCount());
     //printIndex();
