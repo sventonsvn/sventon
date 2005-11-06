c4fcfced7278bc067d1dc7b14966c5a0cebe9f8b
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2005-11-06 23:12:49 +0000

    Added one more test.

--- trunk/dev/java/testsrc/de/berlios/sventon/ctrl/SearchControllerTest.java
+++ trunk/dev/java/testsrc/de/berlios/sventon/ctrl/SearchControllerTest.java
@@ -22,7 +22,7 @@ public class SearchControllerTest extends TestCase {
     req.addParameter("sventonSearchString", "file1");
 
     ctrl.setRevisionIndexer(new RevisionIndexer(SVNRepositoryStub.getInstance()));
-    modelAndView = ctrl.svnHandle(null, command, SVNRevision.HEAD, req, null);
+    modelAndView = ctrl.svnHandle(SVNRepositoryStub.getInstance(), command, SVNRevision.HEAD, req, null);
 
     Map model = modelAndView.getModel();
     List entries = (List) model.get("svndir");
@@ -31,5 +31,13 @@ public class SearchControllerTest extends TestCase {
     assertEquals(2, entries.size());
 
     assertEquals("file", ((RepositoryEntry) entries.get(0)).getKind());
+
+    req = new MockHttpServletRequest();
+    try {
+      modelAndView = ctrl.svnHandle(SVNRepositoryStub.getInstance(), command, SVNRevision.HEAD, req, null);
+      fail("Should throw IllegalArgumentException");
+    } catch (IllegalArgumentException ex) {
+      // expected
+    }
   }
 }
