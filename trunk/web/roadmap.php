83b68dcbc54822613a42a3857f8f7c71e56b5294
patrikfr <patrikfr@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2005-11-20 21:52:37 +0000

    Updated most pages.

--- trunk/web/roadmap.php
+++ trunk/web/roadmap.php
@@ -1,20 +1,4 @@
 <p>
-Mauris ut quam. Sed eu arcu a ipsum tempor nonummy. Proin dignissim, libero in posuere egestas, erat sem luctus dui, ac consequat pede neque posuere mi. Nulla non mi. Nunc mollis, arcu eu mattis pharetra, diam lorem pretium leo, vitae elementum lorem leo vitae sapien. Vivamus ullamcorper tellus suscipit urna. Morbi a lacus. Proin luctus nulla sed leo. Quisque venenatis condimentum lectus. Suspendisse porta nunc in neque. Nunc ac eros et quam dictum bibendum.
-</p>
-
-<p>
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam id libero. Nunc ornare mi et massa. Nulla neque. Nulla id sapien at arcu lacinia ultrices. In feugiat tempus lacus. Duis sit amet magna. Sed ut erat vitae urna ullamcorper luctus. Maecenas ac magna quis turpis ullamcorper volutpat. Praesent ut turpis sed massa pellentesque commodo. Nunc lorem. Pellentesque pellentesque, neque eu rutrum vestibulum, metus purus viverra pede, a viverra erat odio sed orci. Nulla facilisi. Aenean ligula orci, iaculis auctor, scelerisque vitae, pellentesque a, nibh. Curabitur aliquet laoreet tellus.
-</p>
-
-<p>
-Cras eget mauris. Etiam ut ligula. Fusce sit amet pede et eros ornare interdum. Maecenas congue fermentum lectus. Donec id purus. Mauris id dolor. Donec euismod leo. Etiam tristique, leo et porta condimentum, metus risus porttitor felis, ut vulputate lectus nunc ut felis. Praesent sed justo sit amet nunc feugiat placerat. Vivamus blandit nulla. Nunc eros urna, commodo et, tristique eget, iaculis et, nulla. Nullam a nisl a sapien tempus facilisis. Integer condimentum blandit risus. Nulla est felis, posuere nec, interdum quis, adipiscing sed, libero.
-</p>
-
-<p>
-Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce luctus, turpis fringilla blandit bibendum, nisl neque varius leo, sit amet lacinia nulla libero et dui. Etiam neque. Nullam quis urna a metus ornare tempor. In neque. In tincidunt enim congue justo consequat suscipit. Vivamus vehicula odio ac mauris molestie imperdiet. Morbi tellus nisl, eleifend quis, porta eget, nonummy vitae, urna. Phasellus ut lectus vel libero bibendum cursus. Nunc sit amet tellus. Vestibulum posuere dui eget lorem. Aenean tristique lectus id est. Ut sed nibh sit amet risus dignissim elementum. Quisque eget tellus quis justo posuere laoreet. Nam ut felis id felis tristique porta. Nulla at lacus. Sed lorem. Morbi porta. Sed adipiscing augue in lectus.
-</p>
-
-<p>
-Vestibulum consequat. Sed id felis. Vivamus gravida, turpis nec pellentesque scelerisque, lectus lorem mollis neque, vel viverra diam ante at erat. Phasellus feugiat, magna vitae tempor pretium, elit velit dictum ante, vitae elementum turpis nibh quis sem. Sed at odio. Vivamus pellentesque orci eget odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam ipsum erat, viverra sed, ultrices vel, posuere non, velit. Sed eu urna. Praesent interdum interdum lorem.
+Currently, the <a href="http://svn.berlios.de/viewcvs/*checkout*/sventon/trunk/doc/roadmap.txt">roadmap</a> document contains the latest changes and some ideas about upcoming features.
 </p>
 
