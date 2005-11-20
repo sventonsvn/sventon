62a077ecfe6e4c5c520f0b4bc5049e970dee48ce
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2005-11-20 22:05:50 +0000

    Added copyright comment.

--- trunk/dev/java/src/de/berlios/sventon/colorer/Colorer.java
+++ trunk/dev/java/src/de/berlios/sventon/colorer/Colorer.java
@@ -1,3 +1,14 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2005 Sventon Project. All rights reserved.
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution. The terms
+ * are also available at http://sventon.berlios.de.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
 package de.berlios.sventon.colorer;
 
 /**
