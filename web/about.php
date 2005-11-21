c4acbc481c050ef8bb87441567cbfaf2fd7ab61f
Patrik Fredriksson <patrikfr@users.noreply.github.com>
2005-11-21 21:30:20 +0000

    Some updates.

--- web/about.php
+++ web/about.php
@@ -11,9 +11,9 @@ The main goal of the Sventon project is to create a Java web application written
   <ul>
     <li>Insanely easy to install (we're almost there: drop the war file in the application server, enter a few configuration entries on a webpage, restart the application, and your done!).
     <li>Java, and just Java. If you have Java 5 and a web container, you're all set.
-    <li>Neat features such as file search, directory flattening, possiblity to browse archive files such as JAR and ZIP files, thumbnail views of image files, RSS feeds, nice graphs over repository statistics, and more. And the usual stuff, such as logs view, diff and more.
+    <li>Neat features such as file search, directory flattening, possiblity to browse archive files (such as JAR and ZIP files), thumbnail views of image files, RSS feeds, nice graphs over repository statistics, and more. And the usual stuff, such as logs view, diff and more.
   </ul>
-  For the current feature set, have a look at <a href="index.php?page=features">Features</a>.
+  Much of this is now implemented in sventon, for the current feature set, have a look at <a href="index.php?page=features">features</a>.
 </p>
 
 <p>
@@ -25,7 +25,7 @@ The main goal of the Sventon project is to create a Java web application written
 <p>
   <b>Contact</b>
   <br/>
-  If you have any questions regarding sventon usage or development, use the <a href="http://developer.berlios.de/mail/?group_id=3670">mailing lists</a>. Please do not write the developers directly in these matters (but do write to the mailing list, we'll try to answer).
+  If you have any questions regarding sventon usage or development, use the <a href="http://developer.berlios.de/mail/?group_id=3670">mailing lists</a>. Please do not write the developers directly in these matters (but <i>do</i> write to the mailing list, we'll try to answer).
 </p>
 
 <p>
