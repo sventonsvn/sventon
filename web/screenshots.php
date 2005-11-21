c68ad3e697d54fbb10c106b33433c9e514c237ed
Jesper Hammarbäck <jesper@hammarback.eu>
2005-11-21 23:29:37 +0000

    Added screenshot links.

--- web/screenshots.php
+++ web/screenshots.php
@@ -1,8 +1,16 @@
 <p>
-Coming up next...
-</p>
-
-<p>
-(but Sventon is really easy to install, so if you need screenshots now, download a build and try it out) (it's really easy to install) (oh, we already said that)
+  <p><b>Max OS X screenshots</b></p>
+  <table>
+    <tr>
+      <th>Repository browser</th>
+      <th>File diff</th>
+      <th>Logs</th>
+    </tr>
+    <tr>
+      <td><a href="https://developer.berlios.de/dbimage.php?id=2255"><img width="150" height="100" src="https://developer.berlios.de/dbimage.php?id=2255"/></a></td>
+      <td><a href="https://developer.berlios.de/dbimage.php?id=2253"><img width="150" height="100" src="https://developer.berlios.de/dbimage.php?id=2253"/></a></td>
+      <td><a href="https://developer.berlios.de/dbimage.php?id=2254"><img width="150" height="100" src="https://developer.berlios.de/dbimage.php?id=2254"/></a></td>
+    </tr>
+  </table>
 </p>
 
