8489f2146ff791fbff7aac2c45847e3cef8d63ed
Jesper Hammarbäck <jesper@hammarback.eu>
2005-12-03 15:27:39 +0000

    Updated feature list.

--- web/features.php
+++ web/features.php
@@ -9,12 +9,11 @@
   <li>View contents of zip archives.</li>
   <li>View logs history and metadata for files and directories.</li>
   <li>Source code syntax highlighting using <a href="https://jhighlight.dev.java.net">JHighlight</a>.</li>
-  <li>Resource based for easy translation.</li>
   <li>Style sheet marked up for easy graphical customization.</li>
   <li>Single account authentication support for non public repositories.</li>
-  <li>File and directory name search.</li>
+  <li>CamelCase enabled file and directory name search.</li>
   <li>Directory flattening for fast navigation.</li>
   <li>Image thumbnail preview.</li>
-  <li>Keyword expansion when viewing files.</li>
   <li>Diff files between revisions.</li>
+  <li>Keyword expansion when viewing or diffing files.</li>
 </ul>
