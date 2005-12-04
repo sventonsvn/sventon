8c0a566f5561566bf94c664fd1280c4d0d52f9f6
Patrik Fredriksson <patrikfr@users.noreply.github.com>
2005-12-04 18:55:04 +0000

    Copied remotely

---
+++ 0.8/web/screenshots.php
@@ -0,0 +1,16 @@
+<p>
+  <p><b>Max OS X screenshots</b></p>
+  <table>
+    <tr>
+      <th>Repository browser</th>
+      <th>File diff</th>
+      <th>Logs</th>
+    </tr>
+    <tr>
+      <td><a href="http://developer.berlios.de/dbimage.php?id=2255"><img width="150" height="100" src="http://developer.berlios.de/dbimage.php?id=2255"/></a></td>
+      <td><a href="http://developer.berlios.de/dbimage.php?id=2253"><img width="150" height="100" src="http://developer.berlios.de/dbimage.php?id=2253"/></a></td>
+      <td><a href="http://developer.berlios.de/dbimage.php?id=2254"><img width="150" height="100" src="http://developer.berlios.de/dbimage.php?id=2254"/></a></td>
+    </tr>
+  </table>
+</p>
+
