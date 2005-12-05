f03512c01e7c52edaf315727a99fbf83fdc58f6c
patrikfr <patrikfr@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2005-12-05 22:22:11 +0000

    Rel 0.9

---
+++ branches/releases/0.9/web/downloads.php
@@ -0,0 +1,8 @@
+<p>
+Currently there are no pre-built downloads, but if you want to build a release yourself, have a look at: <a href="https://developer.berlios.de/svn/?group_id=3670">https://developer.berlios.de/svn/?group_id=3670</a>
+</p>
+
+<p>
+Pre-built downloads should be available real soon now
+</p>
+
