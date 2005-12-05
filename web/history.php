9671c67f8464abbad26153e9f15629c575dc8995
Patrik Fredriksson <patrikfr@users.noreply.github.com>
2005-12-05 22:28:39 +0000

    Tag 0.9.0

--- web/history.php
+++
@@ -1,4 +0,0 @@
-<p>
-Currently, the <a href="http://svn.berlios.de/viewcvs/*checkout*/sventon/trunk/doc/roadmap.txt">roadmap</a> document contains the latest changes and some ideas about upcoming features.
-</p>
-
