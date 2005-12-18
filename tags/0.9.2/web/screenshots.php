77b2cb7c9606d318b53d4bff18815a4079aeb27d
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2005-12-18 14:56:37 +0000

    Copied remotely

---
+++ tags/0.9.2/web/screenshots.php
@@ -0,0 +1,42 @@
+<p>
+  <b>Mac OS X screenshots</b>
+</p>
+<table>
+  <tr>
+    <th>Repository browser</th>
+    <th>File diff</th>
+    <th>Logs</th>
+  </tr>
+  <tr>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2314"><img width="150" height="100" alt="Repository browser" src="http://developer.berlios.de/dbimage.php?id=2314"/></a></td>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2312"><img width="150" height="100" alt="File diff" src="http://developer.berlios.de/dbimage.php?id=2312"/></a></td>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2313"><img width="150" height="100" alt="Logs" src="http://developer.berlios.de/dbimage.php?id=2313"/></a></td>
+  </tr>
+</table>
+
+<p>
+  <b>Win XP screenshots</b>
+</p>
+<table>
+  <tr>
+    <th>Flattened directory</th>
+    <th>Thumbnail image view</th>
+    <th>Archive file view</th>
+  </tr>
+  <tr>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2316"><img width="150" height="100" alt="Flattened directory" src="http://developer.berlios.de/dbimage.php?id=2316"/></a></td>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2318"><img width="150" height="100" alt="Thumbnail image view" src="http://developer.berlios.de/dbimage.php?id=2318"/></a></td>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2317"><img width="150" height="100" alt="Archive file view" src="http://developer.berlios.de/dbimage.php?id=2317"/></a></td>
+  </tr>
+  <tr>
+    <th>File view</th>
+    <th>File diff</th>
+    <th>&nbsp;</th>
+  </tr>
+  <tr>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2319"><img width="150" height="100" alt="File view" src="http://developer.berlios.de/dbimage.php?id=2319"/></a></td>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2315"><img width="150" height="100" alt="File diff" src="http://developer.berlios.de/dbimage.php?id=2315"/></a></td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
