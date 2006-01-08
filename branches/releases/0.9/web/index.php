5af643924cd0d8945b753853b9de680999bb6717
patrikfr <patrikfr@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2006-01-08 20:31:35 +0000

    Added Google webtracking code.

--- branches/releases/0.9/web/index.php
+++ branches/releases/0.9/web/index.php
@@ -141,7 +141,11 @@
         <td>&nbsp;</td>
       </tr>
     </table>
-
-
-  </body>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-206174-2";
+urchinTracker();
+</script>
+</body>
 </html>
