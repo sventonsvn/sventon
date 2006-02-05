42ad814ea582ad03b8fa6a86497f1674eb37ed32
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2006-02-05 20:52:44 +0000

    Updated.

--- branches/releases/0.9/web/faq.php
+++ branches/releases/0.9/web/faq.php
@@ -5,18 +5,19 @@
   <li><a href="#q4">What platforms are supported?</a></li>
   <li><a href="#q5">What configurations have been tested?</a></li>
   <li><a href="#q6">How do I install and configure sventon?</a></li>
-  <li><a href="#q7">I have followed the installation instructions and restarted the web server but nothing happens when I point my browser to <code>http://yourmachinename/svn/</code>. Why?</a></li>
-  <li><a href="#q8">How do I uninstall sventon?</a></li>
+  <li><a href="#q7">How do I uninstall sventon?</a></li>
+  <li><a href="#q8">How does the search function work?</a></li>
   <li><a href="#q9">How do I upgrade sventon to a newer version?</a></li>
   <li><a href="#q10">Will sventon in any way jeopardize my Subversion repository?</a></li>
   <li><a href="#q11">Will sventon write any information to my Windows registry?</a></li>
   <li><a href="#q12">Why is not file type [XYZ] colorized when displayed in sventon?</a></li>
-  <li><a href="#q13">Can I send suggestions for new functions?</a></li>
-  <li><a href="#q14">Will sventon be avaliable for CVS (or any other version control system) in the future?</a></li>
-  <li><a href="#q15">What license do you use?</a></li>
-  <li><a href="#q16">Why did you start this project?</a></li>
-  <li><a href="#q17">Is there a publicly running version of Sventon that I can try?</a></li>
-  <li><a href="#q18">How can I use Glorbosoft XYZ to maximize team productivity?</a></li>
+  <li><a href="#q13">Is there a log file for sventon?</a></li>
+  <li><a href="#q14">Can I send suggestions for new functions?</a></li>
+  <li><a href="#q15">Will sventon be avaliable for CVS (or any other version control system) in the future?</a></li>
+  <li><a href="#q16">What license do you use?</a></li>
+  <li><a href="#q17">Why did you start this project?</a></li>
+  <li><a href="#q18">Is there a publicly running version of Sventon that I can try?</a></li>
+  <li><a href="#q19">How can I use Glorbosoft XYZ to maximize team productivity?</a></li>
 </ul>
 
 <p>
@@ -102,23 +103,23 @@
 <b>Q:</b> How do I install and configure sventon?
 </a>
 <br/>
-<b>A:</b> <a href="index.php?page=downloads">Download</a> the latest version and drop the <code>WAR</code> file in your servlet container's <code>webapps</code> directory. Point your web browser to <code>http://yourmachinename/svn/</code> and you should see the initial configuration page. If not, make sure your container is running (on the correct port). Submit the fields on the configuration page and restart your servlet container when you are ask to do so. Point your web browser again to <code>http://yourmachinename/svn/</code> and you're done!
+<b>A:</b> <a href="index.php?page=downloads">Download</a> the latest version and drop the <code>WAR</code> file in your servlet container's <code>webapps</code> directory. Point your web browser to <code>http://yourmachinename/svn/</code> and you should see the initial configuration page. If not, make sure your container is running (on the correct port). Submit the fields on the configuration page and you're done! The configuration will be stored in the generated file <code>WEB-INF/classes/sventon.properties</code> and will be used automatically next time the container starts.
 </p>
 
 <p>
 <a name="q7">
-<b>Q:</b> I have followed the installation instructions and restarted the web server but nothing happens when I point my browser to <code>http://yourmachinename/svn/</code>. Why?
-</a>
-<br/>
-<b>A:</b> First time sventon is started the index will be built up. This can take a couple of seconds or even minutes, all depending on network speed and the number of repository entries. Just be patient...
-</p>
-
-<p>
-<a name="q8">
 <b>Q:</b> How do I uninstall sventon?
 </a>
 <br/>
 <b>A:</b> Remove the <code>WAR</code> file from the <code>webapps</code> directory and the subdirectory <code>svn</code>. The index file, <code>sventon.idx</code>, will be kept in a directory specified during the initial configuration. Find it and delete it.
+</p>
+
+<p>
+<a name="q8">
+<b>Q:</b> How does the search function work?
+</a>
+<br/>
+<b>A:</b> The first time sventon is started the repository <tt>HEAD</tt> will be indexed. This can take a couple of minutes depending on network speed and the number of entries. Each time the search or directory flattening functions are used, the index will be updated if necessary. There's also a scheduled job that triggers regularly and ensures that the index is up-to-date. The polling interval can be customized in <code>WEB-INF/sventon-servlet.xml</code>.
 </p>
 
 <p>
@@ -163,14 +164,22 @@ The file extension mapping is done in <code>WEB-INF/sventon-servlet.xml</code> t
 
 <p>
 <a name="q13">
-<b>Q:</b> Can I send suggestions for new functions?
+<b>Q:</b> Is there a log file for sventon?
 </a>
 <br/>
-<b>A:</b> Yes, please do! But we cannot guarantee when or if your suggestion will be implemented
+<b>A:</b> Yes, it's called <code>sventon.log</code> and is by default written to <code>java.io.tmpdir</code>. Logging can be customized by editing the file <code>WEB-INF/classes/log4j.properties</code>.
 </p>
 
 <p>
 <a name="q14">
+<b>Q:</b> Can I send suggestions for new functions?
+</a>
+<br/>
+<b>A:</b> Yes, please do! But we cannot guarantee when or if your suggestion will be implemented.
+</p>
+
+<p>
+<a name="q15">
 <b>Q:</b> Will sventon be avaliable for CVS (or any other version control system) in the future?
 </a>
 <br/>
@@ -178,7 +187,7 @@ The file extension mapping is done in <code>WEB-INF/sventon-servlet.xml</code> t
 </p>
 
 <p>
-<a name="q15">
+<a name="q16">
 <b>Q:</b> What license do you use?
 </a>
 <br/>
@@ -186,7 +195,7 @@ The file extension mapping is done in <code>WEB-INF/sventon-servlet.xml</code> t
 </p>
 
 <p>
-<a name="q16">
+<a name="q17">
 <b>Q:</b> Why did you start this project?
 </a>
 <br/>
@@ -194,7 +203,7 @@ The file extension mapping is done in <code>WEB-INF/sventon-servlet.xml</code> t
 </p>
 
 <p>
-<a name="q17">
+<a name="q18">
 <b>Q:</b> Is there a publicly running version of sventon that I can try?
 </a>
 <br/>
@@ -202,7 +211,7 @@ The file extension mapping is done in <code>WEB-INF/sventon-servlet.xml</code> t
 </p>
 
 <p>
-<a name="q18">
+<a name="q19">
 <b>Q:</b> How can I use Glorbosoft XYZ to maximize team productivity?
 </a>
 <br/>
