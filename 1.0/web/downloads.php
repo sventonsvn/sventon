4513e46ec7b544867578cc041311b39d059a79cd
Jesper Hammarbäck <jesper@hammarback.eu>
2006-02-13 22:51:39 +0000

    Removed file/folder

--- 1.0/web/downloads.php
+++
@@ -1,3 +0,0 @@
-<p>
-Version 0.9.2 is now available for <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v092'); ">download</a>!
-</p>
