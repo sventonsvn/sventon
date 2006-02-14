ec0123d7ba86345e0d110d062cd2d0cab2346383
Jesper Hammarbäck <jesper@hammarback.eu>
2006-02-14 23:13:29 +0000

    Made sure given repository URL does not start or end with whitespace.

--- 1.0/sventon/dev/java/src/de/berlios/sventon/command/ConfigCommandValidator.java
+++ 1.0/sventon/dev/java/src/de/berlios/sventon/command/ConfigCommandValidator.java
@@ -85,9 +85,10 @@ public class ConfigCommandValidator implements Validator {
     String repositoryURL = command.getRepositoryURL();
 
     if (repositoryURL != null) {
+      String trimmedURL = repositoryURL.trim();
       SVNURL url = null;
       try {
-        url = SVNURL.parseURIDecoded(repositoryURL);
+        url = SVNURL.parseURIDecoded(trimmedURL);
       } catch (SVNException ex) {
         String msg = "Invalid repository URL: " + repositoryURL;
         logger.warn(msg);
@@ -96,7 +97,7 @@ public class ConfigCommandValidator implements Validator {
       if (url != null && testConnection) {
         logger.info("Testing repository connection");
         RepositoryConfiguration config = new RepositoryConfiguration();
-        config.setRepositoryRoot(repositoryURL);
+        config.setRepositoryRoot(trimmedURL);
         config.setConfiguredUID(command.getUsername());
         config.setConfiguredPWD(command.getPassword());
         config.setSVNConfigurationPath(command.getConfigPath());
