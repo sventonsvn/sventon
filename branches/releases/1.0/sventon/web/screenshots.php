d5cedb6f41e874161c289425210433998f8c90f0
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2006-02-14 22:37:28 +0000

    Added new images.

--- branches/releases/1.0/sventon/web/screenshots.php
+++ branches/releases/1.0/sventon/web/screenshots.php
@@ -34,9 +34,9 @@
     <th>Archive file view</th>
   </tr>
   <tr>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=2542"><img width="150" height="100" alt="Flattened directory" src="http://developer.berlios.de/dbimage.php?id=2542"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=2544"><img width="150" height="100" alt="Thumbnail image view" src="http://developer.berlios.de/dbimage.php?id=2544"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=2543"><img width="150" height="100" alt="Archive file view" src="http://developer.berlios.de/dbimage.php?id=2543"/></a></td>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2547"><img width="150" height="100" alt="Flattened directory" src="http://developer.berlios.de/dbimage.php?id=2547"/></a></td>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2549"><img width="150" height="100" alt="Thumbnail image view" src="http://developer.berlios.de/dbimage.php?id=2549"/></a></td>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2548"><img width="150" height="100" alt="Archive file view" src="http://developer.berlios.de/dbimage.php?id=2548"/></a></td>
   </tr>
   <tr>
     <th>File view</th>
@@ -44,8 +44,8 @@
     <th>&nbsp;</th>
   </tr>
   <tr>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=2545"><img width="150" height="100" alt="File view" src="http://developer.berlios.de/dbimage.php?id=2545"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=2541"><img width="150" height="100" alt="File diff" src="http://developer.berlios.de/dbimage.php?id=2541"/></a></td>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2551"><img width="150" height="100" alt="File view" src="http://developer.berlios.de/dbimage.php?id=2551"/></a></td>
+    <td><a href="http://developer.berlios.de/dbimage.php?id=2546"><img width="150" height="100" alt="File diff" src="http://developer.berlios.de/dbimage.php?id=2546"/></a></td>
     <td>&nbsp;</td>
   </tr>
 </table>
