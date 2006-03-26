4640f14ea89abaa97c6c9f0b89c56fb1cad2d999
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2006-03-26 19:18:18 +0000

    Removed file/folder

--- branches/releases/1.0/sventon/web/downloads.php
+++
@@ -1,3 +0,0 @@
-<p>
-Version 1.0 RC1 is now available for <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v10rc1'); ">download</a>!
-</p>
