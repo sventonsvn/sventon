65ab6e6550311b7bfecc0a42d90df980b9263202
Jesper Hammarbäck <jesper@hammarback.eu>
2006-03-28 22:43:42 +0000

    Refactoring of feed handlers.

--- 1.0/sventon/dev/java/testsrc/de/berlios/sventon/rss/AllTests.java
+++ 1.0/sventon/dev/java/testsrc/de/berlios/sventon/rss/AllTests.java
@@ -11,7 +11,7 @@ public class AllTests {
 
   public static Test suite() {
     TestSuite suite = new TestSuite("Test for de.berlios.sventon.rss");
-    suite.addTestSuite(FeedGeneratorTest.class);
+    suite.addTestSuite(SyndFeedGeneratorTest.class);
     return suite;
   }
 
