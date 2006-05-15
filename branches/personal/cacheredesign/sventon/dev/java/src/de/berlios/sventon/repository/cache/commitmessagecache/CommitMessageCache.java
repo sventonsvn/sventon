6d53192842d2e6e197cb21898047a1c96ff02e45
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2006-05-15 19:43:18 +0000

    More work on lucene cache.

--- branches/personal/cacheredesign/sventon/dev/java/src/de/berlios/sventon/repository/cache/commitmessagecache/CommitMessageCache.java
+++ branches/personal/cacheredesign/sventon/dev/java/src/de/berlios/sventon/repository/cache/commitmessagecache/CommitMessageCache.java
@@ -12,7 +12,7 @@
 package de.berlios.sventon.repository.cache.commitmessagecache;
 
 import de.berlios.sventon.repository.cache.CacheException;
-import org.tmatesoft.svn.core.SVNLogEntry;
+import de.berlios.sventon.repository.CommitMessage;
 
 import java.util.List;
 
@@ -27,60 +27,28 @@ public interface CommitMessageCache {
   /**
    * Finds occurencies of given search string among the cached commit messages.
    *
-   * @param searchString String to search for
-   * @return List of something. Kind of depends on Lucene implementation. -- REVISIT
+   * @param queryString Index query string.
+   * @return List of commit messages.
    * @throws de.berlios.sventon.repository.cache.CacheException
-   */
-  List<Object> find(final String searchString) throws CacheException;
-
-  /**
-   * Add one log entry to the cache.
    *
-   * @param entry The entry to parse and add
    */
-  void add(final SVNLogEntry entry);
+  List<CommitMessage> find(final String queryString) throws CacheException;
 
   /**
-   * Add one or more log entries to the cache.
+   * Add one commit message to the cache.
    *
-   * @param entries The entries to parse and add
+   * @param commitMessage The commit message to cache.
+   * @throws CacheException if error during index addition.
    */
-  void add(final SVNLogEntry... entries);
+  void add(final CommitMessage commitMessage) throws CacheException;
 
   /**
-   * Gets the cached revision number.
-   * Used to determine if cache needs to be updated.
+   * Gets the size of the commit message cache, i.e. the number
+   * of cached messages.
    *
-   * @return Cached revision number.
+   * @return Cache size
+   * @throws CacheException if unable to get cache size
    */
-  long getCachedRevision();
-
-  /**
-   * Sets the cached revision number.
-   * Used if cache has been updated.
-   *
-   * @param revision Revision number.
-   */
-  void setCachedRevision(final long revision);
-
-  /**
-   * Clears the entire cache.
-   */
-  void clear();
-
-  /**
-   * Gets the repository URL.
-   * Used to verifiy that the cache state and url matches.
-   *
-   * @return The URL to the repository.
-   */
-  String getRepositoryUrl();
-
-  /**
-   * Sets the repository URL.
-   *
-   * @param repositoryURL The URL
-   */
-  void setRepositoryURL(final String repositoryURL);
+  int getSize() throws CacheException;
 
 }
---
+++ branches/personal/cacheredesign/sventon/dev/java/testsrc/de/berlios/sventon/repository/cache/commitmessagecache/CommitMessageCacheTest.java
@@ -0,0 +1,58 @@
+package de.berlios.sventon.repository.cache.commitmessagecache;
+
+import de.berlios.sventon.repository.CommitMessage;
+import de.berlios.sventon.repository.cache.CacheException;
+import junit.framework.TestCase;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
+public class CommitMessageCacheTest extends TestCase {
+
+  public void testFindWhenEmpty() throws Exception {
+    final Directory directory = new RAMDirectory();
+    final CommitMessageCacheImpl cache = new CommitMessageCacheImpl(directory);
+
+    try {
+      cache.find("test");
+      fail("Should cause CacheException");
+    } catch (CacheException ce) {
+      // expected
+    }
+    directory.close();
+  }
+
+  public void testAdd() throws Exception {
+    final Directory directory = new RAMDirectory();
+    final CommitMessageCacheImpl cache = new CommitMessageCacheImpl(directory);
+
+    cache.add(new CommitMessage(123, "This is a commit message for revision 123."));
+    assertEquals(1, cache.getSize());
+
+    assertEquals(1, cache.find("123").size());
+
+    cache.add(new CommitMessage(124, "This is a commit message for revision 124."));
+    assertEquals(2, cache.getSize());
+
+    assertEquals(2, cache.find("message").size());
+    cache.add(new CommitMessage(125, "This is a commit message for revision 125."));
+    assertEquals(3, cache.getSize());
+
+    cache.add(new CommitMessage(126, "This is a commit message for revision 126."));
+    assertEquals(4, cache.getSize());
+
+    assertEquals(4, cache.find("message").size());
+
+    directory.close();
+  }
+
+  public void testAddEmptyAndNull() throws Exception {
+    final Directory directory = new RAMDirectory();
+    final CommitMessageCacheImpl cache = new CommitMessageCacheImpl(directory);
+
+    cache.add(new CommitMessage(123, "this is"));
+    assertEquals(1, cache.getSize());
+
+    directory.close();
+  }
+
+}
