fdf3405981feac859ea7a8171b8f5caeb376e09e
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2006-08-24 20:26:40 +0000

    Changed max length of display friendly entry name from 60 to 70.

--- trunk/sventon/dev/java/testsrc/de/berlios/sventon/repository/RepositoryEntryTest.java
+++ trunk/sventon/dev/java/testsrc/de/berlios/sventon/repository/RepositoryEntryTest.java
@@ -11,12 +11,12 @@ public class RepositoryEntryTest extends TestCase {
   public void testGetFriendlyFullEntryNameLongFullName() throws Exception {
     SVNDirEntry e = new SVNDirEntry(null, "test.fil", SVNNodeKind.FILE, 1, false, 1, new Date(), "A");
     RepositoryEntry entry = new RepositoryEntry(e, "/source/com/bli/bla/blu/saaaaaoooooooooooaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/", null);
-    assertEquals(".../saaaaaoooooooooooaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/test.fil", entry.getFriendlyFullEntryName());
+    assertEquals("...li/bla/blu/saaaaaoooooooooooaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/test.fil", entry.getFriendlyFullEntryName());
     assertEquals(RepositoryEntry.FULL_ENTRY_NAME_MAX_LENGTH, entry.getFriendlyFullEntryName().length());
 
     e = new SVNDirEntry(null, "reallylongfilenamejustfortestingpurposes.fil", SVNNodeKind.FILE, 1, false, 1, new Date(), "A");
     entry = new RepositoryEntry(e, "/source/com/bli/bla/blu/saaaaa/bbbbbb/ccccccccc/dddddddd/aaaaaaaaaaaaaaaaaa/", null);
-    assertEquals("...aaaaaaaaaaaa/reallylongfilenamejustfortestingpurposes.fil", entry.getFriendlyFullEntryName());
+    assertEquals("...ddd/aaaaaaaaaaaaaaaaaa/reallylongfilenamejustfortestingpurposes.fil", entry.getFriendlyFullEntryName());
     assertEquals(RepositoryEntry.FULL_ENTRY_NAME_MAX_LENGTH, entry.getFriendlyFullEntryName().length());
   }
 
@@ -27,15 +27,15 @@ public class RepositoryEntryTest extends TestCase {
   }
 
   public void testGetFriendlyFullEntryNameLongName() throws Exception {
-    SVNDirEntry e = new SVNDirEntry(null, "thisisafilenamewithmorethanfiftycharactersinitreallynotperfectlookingontheweb.fil", SVNNodeKind.FILE, 1,
+    SVNDirEntry e = new SVNDirEntry(null, "thisisafilenamewithmorethanenoughcharactersinitreallynotperfectlookingontheweb.fil", SVNNodeKind.FILE, 1,
         false, 1, new Date(), "A");
     RepositoryEntry entry = new RepositoryEntry(e, "/source/com/bli/bla/blu/", null);
-    assertEquals("...hanfiftycharactersinitreallynotperfectlookingontheweb.fil", entry.getFriendlyFullEntryName());
+    assertEquals("...withmorethanenoughcharactersinitreallynotperfectlookingontheweb.fil", entry.getFriendlyFullEntryName());
     assertEquals(RepositoryEntry.FULL_ENTRY_NAME_MAX_LENGTH, entry.getFriendlyFullEntryName().length());
 
     e = new SVNDirEntry(null, "reallylongfilenamejustfortestingpurposesonly.fil", SVNNodeKind.FILE, 1, false, 1, new Date(), "A");
     entry = new RepositoryEntry(e, "/source/com/bli/bla/blu/saaaaa/bbbbbb/ccccccccc/dddddddd/aaaaaaaaaaaaaaaaaa/", null);
-    assertEquals("...aaaaaaaa/reallylongfilenamejustfortestingpurposesonly.fil", entry.getFriendlyFullEntryName());
+    assertEquals("...aaaaaaaaaaaaaaaaaa/reallylongfilenamejustfortestingpurposesonly.fil", entry.getFriendlyFullEntryName());
     assertEquals(RepositoryEntry.FULL_ENTRY_NAME_MAX_LENGTH, entry.getFriendlyFullEntryName().length());
   }
 
