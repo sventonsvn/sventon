45447762f876bb5a5a1f090f1a95937311b52512
Jesper Hammarbäck <jesper@hammarback.eu>
2006-09-15 23:08:50 +0000

    JSP refactoring. Fixed misspelled view name.

--- sventon/dev/java/src/de/berlios/sventon/web/ctrl/ShowLocksController.java
+++ sventon/dev/java/src/de/berlios/sventon/web/ctrl/ShowLocksController.java
@@ -46,7 +46,7 @@ public class ShowLocksController extends AbstractSVNTemplateController implement
 
     final Map<String, Object> model = new HashMap<String, Object>();
     model.put("currentLocks", getLocks(repository, svnCommand.getPath()).values());
-    return new ModelAndView("showlocks", model);
+    return new ModelAndView("showLocks", model);
   }
 
 }
