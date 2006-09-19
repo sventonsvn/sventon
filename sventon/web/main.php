394973278d4fa5a166661bab150f4599f02b22e6
Jesper Hammarbäck <jesper@hammarback.eu>
2006-09-19 12:19:04 +0000

    Updated feature list.

--- sventon/web/main.php
+++ sventon/web/main.php
@@ -7,14 +7,15 @@ sventon is a Java web application written for browsing <a href="http://subversio
 </p>
 
 <p>
-sventon is really easy to install, all you need is a <a href="http://java.sun.com/j2se/1.5.0/">J2SE 5.0 runtime</a> and a Servlet 2.4/JSP 2.0 compliant webserver, such as <a href="http://tomcat.apache.org/">Tomcat 5.5</a>. It features most basic functionality you'd expect from a tool like this, and a few more, including:
+sventon is really easy to install and you don't have to know anything about Java. All you need is a <a href="http://java.sun.com/j2se/1.5.0/">J2SE 5.0 runtime</a> and a Servlet 2.4/JSP 2.0 compliant webserver, such as <a href="http://tomcat.apache.org/">Tomcat 5.5</a>. It features most basic functionality you'd expect from a tool like this, and a few more, including:
 </p>
 
 <ul>
   <li>Easy installation</li>
   <li>Browse, download, view logs and diff your repository files, and their properties</li>
+  <li>Support for multiple repositories</li>
   <li>Easily keep track of changes using the 'latest commit' feature</li>
-  <li>Lightning fast search for instantly finding files, including CamelCaseSearch</li>
+  <li>Lightning fast search for instantly finding files or log messages, including CamelCaseSearch</li>
   <li>Flattening of directory structures for quickly finding the directory you're looking for</li>
   <li>Browse inside archive files</li>
   <li>View thumbnails of image files stored in the repository</li>
