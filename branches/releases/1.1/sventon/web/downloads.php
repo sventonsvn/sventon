80646317979739d4ece576897d1290b5ffd5a105
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2006-10-30 22:34:48 +0000

    Updated web for version 1.1 RC1.

--- branches/releases/1.1/sventon/web/downloads.php
+++ branches/releases/1.1/sventon/web/downloads.php
@@ -1,3 +1,3 @@
 <p>
-Version 1.0.0 is now available for <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v100'); ">download</a>!
+Version 1.1 RC1 is now available for <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v11rc1'); ">download</a>!
 </p>
