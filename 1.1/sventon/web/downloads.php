8aa2c3ed942700cd8c6bee12719133c6b48b7dfa
Jesper Hammarbäck <jesper@hammarback.eu>
2006-11-06 18:35:50 +0000

    Updated with link to RC2.

--- 1.1/sventon/web/downloads.php
+++ 1.1/sventon/web/downloads.php
@@ -3,7 +3,7 @@ Available versions:
 <p>
 
 <ul>
-  <li><a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v11rc1'); ">1.1 RC1 (beta)</a></li>
+  <li><a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v11rc2'); ">1.1 RC2 (beta)</a></li>
   <li><a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v100'); ">1.0.0 (stable)</a></li>
 </ul>
 
