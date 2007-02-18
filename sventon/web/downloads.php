34ec1620749f4a03aa3624f95646146069d68938
Jesper Hammarbäck <jesper@hammarback.eu>
2007-02-18 22:47:16 +0000

    Merge from 1.1 branch.

--- sventon/web/downloads.php
+++ sventon/web/downloads.php
@@ -3,8 +3,7 @@ Available versions:
 <p>
 
 <ul>
-  <li><a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v11rc4'); ">1.1 RC4 (beta)</a></li>
-  <li><a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v100'); ">1.0.0 (stable)</a></li>
+  <li><a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v110'); ">1.1.0 (stable)</a></li>
 </ul>
 
 </p>
