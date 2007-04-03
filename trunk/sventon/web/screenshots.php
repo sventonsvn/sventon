37edcbea151b07d9a8ca394879c4ee84ce7ec40f
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2007-04-03 20:12:12 +0000

    Moved screenshots from BerliOS to the sventon website. Added screenshot images to repository.

--- trunk/sventon/web/screenshots.php
+++ trunk/sventon/web/screenshots.php
@@ -8,9 +8,9 @@
     <th>Archive file view</th>
   </tr>
   <tr>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3258"><img width="150" height="100" alt="Flattened directory" src="http://developer.berlios.de/dbimage.php?id=3258"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3260"><img width="150" height="100" alt="Thumbnail image view" src="http://developer.berlios.de/dbimage.php?id=3260"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3255"><img width="150" height="100" alt="Archive file view" src="http://developer.berlios.de/dbimage.php?id=3255"/></a></td>
+    <td><a href="img/screenshots/mac_flatten.png"><img width="150" height="100" alt="Flattened directory" src="img/screenshots/mac_flatten.png"/></a></td>
+    <td><a href="img/screenshots/mac_thumbnail.png"><img width="150" height="100" alt="Thumbnail image view" src="img/screenshots/mac_thumbnail.png"/></a></td>
+    <td><a href="img/screenshots/mac_archive.png"><img width="150" height="100" alt="Archive file view" src="img/screenshots/mac_archive.png"/></a></td>
   </tr>
   <tr>
     <th>File view</th>
@@ -18,9 +18,9 @@
     <th>Search logs</th>
   </tr>
   <tr>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3257"><img width="150" height="100" alt="File view" src="http://developer.berlios.de/dbimage.php?id=3257"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3256"><img width="150" height="100" alt="File diff" src="http://developer.berlios.de/dbimage.php?id=3256"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3259"><img width="150" height="100" alt="Search logs" src="http://developer.berlios.de/dbimage.php?id=3259"/></a></td>
+    <td><a href="img/screenshots/mac_showfile.png"><img width="150" height="100" alt="File view" src="img/screenshots/mac_showfile.png"/></a></td>
+    <td><a href="img/screenshots/mac_diff.png"><img width="150" height="100" alt="File diff" src="img/screenshots/mac_diff.png"/></a></td>
+    <td><a href="img/screenshots/mac_logsearch.png"><img width="150" height="100" alt="Search logs" src="img/screenshots/mac_logsearch.png"/></a></td>
   </tr>
 </table>
 
@@ -34,9 +34,9 @@
     <th>Archive file view</th>
   </tr>
   <tr>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3252"><img width="150" height="100" alt="Flattened directory" src="http://developer.berlios.de/dbimage.php?id=3252"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3250"><img width="150" height="100" alt="Thumbnail image view" src="http://developer.berlios.de/dbimage.php?id=3250"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3251"><img width="150" height="100" alt="Archive file view" src="http://developer.berlios.de/dbimage.php?id=3251"/></a></td>
+    <td><a href="img/screenshots/win_flatten.png"><img width="150" height="100" alt="Flattened directory" src="img/screenshots/win_flatten.png"/></a></td>
+    <td><a href="img/screenshots/win_thumbnail.png"><img width="150" height="100" alt="Thumbnail image view" src="img/screenshots/win_thumbnail.png"/></a></td>
+    <td><a href="img/screenshots/win_archive.png"><img width="150" height="100" alt="Archive file view" src="img/screenshots/win_archive.png"/></a></td>
   </tr>
   <tr>
     <th>File view</th>
@@ -44,9 +44,9 @@
     <th>Search logs</th>
   </tr>
   <tr>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3249"><img width="150" height="100" alt="File view" src="http://developer.berlios.de/dbimage.php?id=3249"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3253"><img width="150" height="100" alt="File diff" src="http://developer.berlios.de/dbimage.php?id=3253"/></a></td>
-    <td><a href="http://developer.berlios.de/dbimage.php?id=3254"><img width="150" height="100" alt="Search logs" src="http://developer.berlios.de/dbimage.php?id=3254"/></a></td>
+    <td><a href="img/screenshots/win_showfile.png"><img width="150" height="100" alt="File view" src="img/screenshots/win_showfile.png"/></a></td>
+    <td><a href="img/screenshots/win_diff.png"><img width="150" height="100" alt="File diff" src="img/screenshots/win_diff.png"/></a></td>
+    <td><a href="img/screenshots/win_logsearch.png"><img width="150" height="100" alt="Search logs" src="img/screenshots/win_logsearch.png"/></a></td>
     <td>&nbsp;</td>
   </tr>
 </table>
