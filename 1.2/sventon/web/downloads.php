ee9663ce97afa53dba3968a6ac62c8fa6040b3a9
Jesper Hammarbäck <jesper@hammarback.eu>
2007-04-23 19:15:00 +0000

    Added links to alternative download locations.

--- 1.2/sventon/web/downloads.php
+++ 1.2/sventon/web/downloads.php
@@ -7,4 +7,15 @@ Available versions:
   <li><a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v110'); ">1.1.0 (stable)</a></li>
 </ul>
 
+<p>
+  Alternative download locations:
+</p>
+
+<ul>
+  <li><a href="/download/sventon-1.2rc1-r825-bin.zip" onClick="javascript:urchinTracker ('/downloads/v12rc1'); ">1.2 RC1 (beta) binary</a></li>
+  <li><a href="/download/sventon-1.2rc1-r825-src.zip" onClick="javascript:urchinTracker ('/downloads/v12rc1'); ">1.2 RC1 (beta) source</a></li>
+  <li><a href="/download/sventon-1.1.0-r794-bin.zip" onClick="javascript:urchinTracker ('/downloads/v110'); ">1.1.0 (stable) binary</a></li>
+  <li><a href="/download/sventon-1.1.0-r794-src.zip" onClick="javascript:urchinTracker ('/downloads/v110'); ">1.1.0 (stable) source</a></li>
+</ul>
+
 </p>
