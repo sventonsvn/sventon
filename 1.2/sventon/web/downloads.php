6ab352b8eed01a43c9e4f68bacc0edc4ab10954b
Jesper Hammarbäck <jesper@hammarback.eu>
2007-05-17 22:58:29 +0000

    Prepared for release of sventon 1.2 RC2.

--- 1.2/sventon/web/downloads.php
+++ 1.2/sventon/web/downloads.php
@@ -3,7 +3,7 @@ Available versions:
 <p>
 
 <ul>
-  <li><a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v12rc1'); ">1.2 RC1 (beta)</a></li>
+  <li><a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v12rc2'); ">1.2 RC2 (beta)</a></li>
   <li><a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v110'); ">1.1.0 (stable)</a></li>
 </ul>
 
@@ -12,8 +12,8 @@ Available versions:
 </p>
 
 <ul>
-  <li><a href="/download/sventon-1.2rc1-r825-bin.zip" onClick="javascript:urchinTracker ('/downloads/v12rc1'); ">1.2 RC1 (beta) binary</a></li>
-  <li><a href="/download/sventon-1.2rc1-r825-src.zip" onClick="javascript:urchinTracker ('/downloads/v12rc1'); ">1.2 RC1 (beta) source</a></li>
+  <li><a href="/download/sventon-1.2rc2-r836-bin.zip" onClick="javascript:urchinTracker ('/downloads/v12rc2'); ">1.2 RC2 (beta) binary</a></li>
+  <li><a href="/download/sventon-1.2rc2-r836-src.zip" onClick="javascript:urchinTracker ('/downloads/v12rc2'); ">1.2 RC2 (beta) source</a></li>
   <li><a href="/download/sventon-1.1.0-r794-bin.zip" onClick="javascript:urchinTracker ('/downloads/v110'); ">1.1.0 (stable) binary</a></li>
   <li><a href="/download/sventon-1.1.0-r794-src.zip" onClick="javascript:urchinTracker ('/downloads/v110'); ">1.1.0 (stable) source</a></li>
 </ul>
