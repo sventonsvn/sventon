69615bc41231ff24ced353241640650504ad4dcf
Patrik Fredriksson <patrikfr@users.noreply.github.com>
2007-10-02 21:05:37 +0000

    Added Development page.

---
+++ sventon/new_web/development.php
@@ -0,0 +1,50 @@
+<div class="header">sventon development</div>
+<div class="body">
+  <b>Monitoring progress</b>
+  <p>
+  sventon is an open source project licensed under the
+  <a href="http://svn.berlios.de/svnroot/repos/sventon/trunk/sventon/licenses/LICENSE">new BSD license</a>. You can
+  follow the progress of the project in different ways, including:
+  </p>
+  <p>
+  <b><a href="http://developer.berlios.de/projects/sventon/">The project page at BerliOS</a></b><br/>
+  Here you will find source code repository, bug and feature request databases and releases.
+  </p>
+  <p>
+  <b><a href="http://svn.sventon.org">The sventon sventon instance</a></b><br/>
+  Browse the source code and keep track of changes using the RSS feeds
+  </p>
+  <p>
+  <b><a href="http://svn.sventon.org/repobrowser.svn?name=berlios">The sventon sventon instance</a></b><br/>
+  Browse the source code and keep track of changes using the RSS feeds
+  </p>
+  <p>
+  <b><a href="http://groups.google.com/group/sventon-support/">The sventon support Google group</a></b><br/>
+  Discuss usage and development with sventon committers and users.  Bugs and feature requests submitted to
+  <a href="http://developer.berlios.de/projects/sventon/">trackers at BerliOS</a> are posted here as well.
+  </p>
+
+  <br/>
+  <b>Contributing</b>
+  <p>
+  If you would like to contribute we are happy to accept bug reports, feature requests and patches!
+  If you fix bugs and/or build extensions to sventon that you would like to contribute back to the project
+  we will be happy to accept such contributions if we find them in line with where we think sventon is going.
+  </p>
+  <p>
+  Please have a look at <a href="http://developer.berlios.de/feature/?group_id=3670">
+  http://developer.berlios.de/feature/?group_id=3670</a> for some ideas of features we are
+  considering.
+  </p>
+  <p>
+  The latest development sources can be accessed anonymously from
+  <code>http://svn.berlios.de/svnroot/repos/sventon/trunk</code>.
+  </p>
+  <p>
+  The best way of communicating would be to use our
+  <a href="http://groups.google.com/group/sventon-support/">Google group</a>. Patches can be
+  <a href="http://groups.google.com/group/sventon-support/files?upload=1">uploaded to the group</a> or emailed directly
+  to <a href="mailto:sventonproject@gmail.com">sventonproject@gmail.com</a>
+  </p>
+
+</div>
