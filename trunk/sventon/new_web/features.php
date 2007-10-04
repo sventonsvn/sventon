b58ecb72a4f04c86b086f66dcaba94820b8b56f2
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2007-10-04 21:33:51 +0000

    Updated page.

--- trunk/sventon/new_web/features.php
+++ trunk/sventon/new_web/features.php
@@ -4,9 +4,9 @@
   <li>Easy installation and setup.</li>
   <li>WebDAV (http/https), svnserve (svn/svn+ssh) and file repository browsning.</li>
   <li>Support for multiple repositories.</li>
-  <li>View and download files for any given revision.</li>
+  <li>View and download files for any given revision or date in time.</li>
   <li>Sort or filter directory listings.</li>
-  <li>View contents of archives.</li>
+  <li>View contents of archives and files inside archives.</li>
   <li>View logs history and metadata for files and directories.</li>
   <li>Log cache powered by <a href="http://lucene.apache.org/">Lucene</a>.</li>
   <li>Source code syntax highlighting using <a href="https://jhighlight.dev.java.net">JHighlight</a>.</li>
