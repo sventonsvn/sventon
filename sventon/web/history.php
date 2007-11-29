a4b54cd995b548b7c2de9930a4d1cdee0c8a8657
Jesper Hammarbäck <jesper@hammarback.eu>
2007-11-29 21:56:09 +0000

    Deleted old web pages.

--- sventon/web/history.php
+++
@@ -1,4 +0,0 @@
-<p>
-See the <a href="http://svn.berlios.de/viewcvs/*checkout*/sventon/branches/releases/1.2/sventon/doc/changes.txt">change history</a> document for version details.
-</p>
-
--- sventon/web/roadmap.php
+++
@@ -1,4 +0,0 @@
-<p>
-Currently, the <a href="http://developer.berlios.de/feature/?group_id=3670">feature request</a> page contains the some ideas about upcoming features.
-</p>
-
