2696101fa4a2698214daf854982c311e28330003
Jesper Hammarbäck <jesper@hammarback.eu>
2007-12-13 16:58:04 +0000

    Inlined method and moved tests.

--- sventon/src/test/java/de/berlios/sventon/util/EncodingUtilsTest.java
+++ sventon/src/test/java/de/berlios/sventon/util/EncodingUtilsTest.java
@@ -9,4 +9,12 @@ public class EncodingUtilsTest extends TestCase {
     assertEquals("+", EncodingUtils.encode(" "));
   }
 
+  public void testEncodeUrl() throws Exception {
+    assertEquals("http://localhost/svn/repobrowser.svn?path=/trunk&revision=HEAD&name=test",
+        EncodingUtils.encodeUrl("http://localhost/svn/repobrowser.svn?path=%2ftrunk&revision=HEAD&name=test"));
+
+    assertEquals("http://localhost/svn/showfile.svn?path=/trunk/public/%c3%a5%c3%a4%c3%b6.txt&revision=HEAD&name=test",
+        EncodingUtils.encodeUrl("http://localhost/svn/showfile.svn?path=%2ftrunk%2fpublic%2f%c3%a5%c3%a4%c3%b6.txt&revision=HEAD&name=test"));
+  }
+
 }
