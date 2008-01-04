70116cb43978ffde81a093c057261aef0b635afe
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2008-01-04 13:26:26 +0000

    Renamed attribute.

--- trunk/sventon/src/main/java/de/berlios/sventon/web/ctrl/DiffPreviousController.java
+++ trunk/sventon/src/main/java/de/berlios/sventon/web/ctrl/DiffPreviousController.java
@@ -55,7 +55,7 @@ public final class DiffPreviousController extends AbstractSVNTemplateController 
 
     //TODO: Solve this issue in a better way?
     if (SVNNodeKind.NONE == getRepositoryService().getNodeKind(repository, svnCommand.getPath(), commitRev)) {
-      model.put("isMissingHistory", true);
+      model.put("noHistory", true);
     } else {
       final List<SVNFileRevision> revisions =
           getRepositoryService().getFileRevisions(repository, svnCommand.getPath(), commitRev);
