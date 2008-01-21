6621555a1ba9c4f73fdd1fa689ff7c47cc9e307a
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2008-01-21 21:27:48 +0000

    Added 'configFile' parameter to be displayed in the confirmAddConfig view.

--- trunk/sventon/src/main/java/de/berlios/sventon/web/ctrl/ConfigurationController.java
+++ trunk/sventon/src/main/java/de/berlios/sventon/web/ctrl/ConfigurationController.java
@@ -70,22 +70,24 @@ public final class ConfigurationController extends AbstractFormController {
 
     logger.debug("showForm() started");
     logger.info("sventon configured: " + application.isConfigured());
+
     if (application.isConfigured()) {
-      // sventon already configured - return to browser view.
       logger.debug("Already configured - returning to browser view");
       return new ModelAndView(new RedirectView("repobrowser.svn"));
-    } else if (!application.getInstanceNames().isEmpty() && request.getParameter("addnew") == null) {
+    }
+
+    final Map<String, Object> model = new HashMap<String, Object>();
+    model.put("addedInstances", application.getInstanceNames());
+
+    if (!application.getInstanceNames().isEmpty() && request.getParameter("addnew") == null) {
       //Config url is invoked with at least one repository already added
-      final Map<String, Object> model = new HashMap<String, Object>();
-      model.put("addedInstances", application.getInstanceNames());
       return new ModelAndView("confirmAddConfig", model);
     } else {
       //Config URL invoked for the first time in a non-configured sventon instance
-      final Map<String, Object> model = new HashMap<String, Object>();
       final ConfigCommand configCommand = new ConfigCommand();
       logger.debug("'command' set to: " + configCommand);
       model.put("command", configCommand);
-      model.put("addedInstances", application.getInstanceNames());
+
       logger.debug("Displaying the config page");
       return new ModelAndView("config", model);
     }
@@ -99,13 +101,16 @@ public final class ConfigurationController extends AbstractFormController {
                                                final BindException errors) throws IOException {
 
     logger.debug("processFormSubmission() started");
-    final Map<String, Object> model = new HashMap<String, Object>();
     logger.info("sventon configuration OK: " + application.isConfigured());
+
     if (application.isConfigured()) {
-      // sventon already configured - return to browser view.
       logger.debug("Already configured - returning to browser view");
       return new ModelAndView(new RedirectView("repobrowser.svn"));
     }
+
+    final Map<String, Object> model = new HashMap<String, Object>();
+    model.put("addedInstances", application.getInstanceNames());
+    model.put("configFile", application.getConfigurationFile());
 
     final ConfigCommand confCommand = (ConfigCommand) command;
 
@@ -113,13 +118,10 @@ public final class ConfigurationController extends AbstractFormController {
       //noinspection unchecked
       model.putAll(errors.getModel());
       model.put("command", confCommand);
-      model.put("addedInstances", application.getInstanceNames());
       return new ModelAndView("config", model);
-
     } else {
       final InstanceConfiguration instanceConfiguration = confCommand.createInstanceConfiguration();
       application.addInstance(instanceConfiguration);
-      model.put("addedInstances", application.getInstanceNames());
       model.put("latestAddedInstance", confCommand.getName());
       return new ModelAndView("confirmAddConfig", model);
     }
