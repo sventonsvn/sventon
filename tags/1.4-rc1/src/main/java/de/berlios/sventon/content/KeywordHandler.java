8693c7f2b8d7d343e81c3e26395eda18f2e41968
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2008-02-19 20:45:29 +0000

    [maven-release-plugin]  copy for tag 1.4-rc1

---
+++ tags/1.4-rc1/src/main/java/de/berlios/sventon/content/KeywordHandler.java
@@ -0,0 +1,99 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2005-2008 sventon project. All rights reserved.
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution. The terms
+ * are also available at http://sventon.berlios.de.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package de.berlios.sventon.content;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.tmatesoft.svn.core.SVNProperty;
+import org.tmatesoft.svn.core.internal.wc.admin.SVNTranslator;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * Handler class for subversion keyword substitution.
+ * <p/>
+ * Valid keywords are:
+ * <table>
+ * <tr><td>Date</td></tr>
+ * <tr><td>LastChangedDate</td></tr>
+ * <tr><td>Rev</td></tr>
+ * <tr><td>Revision</td></tr>
+ * <tr><td>LastChangedRevision</td></tr>
+ * <tr><td>Author</td></tr>
+ * <tr><td>LastChangedBy</td></tr>
+ * <tr><td>URL</td></tr>
+ * <tr><td>HeadURL</td></tr>
+ * <tr><td>Id</td></tr>
+ * </table>
+ *
+ * @author jesper@users.berlios.de
+ * @see org.tmatesoft.svn.core.internal.wc.admin.SVNTranslator
+ */
+public final class KeywordHandler {
+
+  public static final String KEYWORD_LAST_CHANGED_DATE = "LastChangedDate";
+  public static final String KEYWORD_DATE = "Date";
+  public static final String KEYWORD_LAST_CHANGED_REVISION = "LastChangedRevision";
+  public static final String KEYWORD_REVISION = "Revision";
+  public static final String KEYWORD_REV = "Rev";
+  public static final String KEYWORD_LAST_CHANGED_BY = "LastChangedBy";
+  public static final String KEYWORD_AUTHOR = "Author";
+  public static final String KEYWORD_HEAD_URL = "HeadURL";
+  public static final String KEYWORD_URL = "URL";
+  public static final String KEYWORD_ID = "Id";
+
+  private final Map<String, byte[]> keywordsMap;
+
+  /**
+   * The logging instance.
+   */
+  private final Log logger = LogFactory.getLog(getClass());
+
+  /**
+   * Constructs the instance and computes keyword variables.
+   *
+   * @param properties The subversion entry's properties.
+   * @param url        The full url to the repository entry
+   */
+  @SuppressWarnings({"unchecked"})
+  public KeywordHandler(final Map properties, final String url) {
+    final String author = (String) properties.get(SVNProperty.LAST_AUTHOR);
+    final String date = (String) properties.get(SVNProperty.COMMITTED_DATE);
+    final String revision = (String) properties.get(SVNProperty.COMMITTED_REVISION);
+    final String keywords = (String) properties.get(SVNProperty.KEYWORDS);
+    keywordsMap = SVNTranslator.computeKeywords(keywords, url, author, date, revision, null);
+  }
+
+  /**
+   * Substitutes keywords in content.
+   *
+   * @param content  The content
+   * @param encoding Encoding to use.
+   * @return Content with substituted keywords.
+   * @throws UnsupportedEncodingException if given encoding is unsupported.
+   */
+  public String substitute(final String content, final String encoding) throws UnsupportedEncodingException {
+    logger.debug("Substituting keywords");
+    String substitutedContent = content;
+    for (String keyword : keywordsMap.keySet()) {
+      final String value = new String(keywordsMap.get(keyword), encoding);
+      logger.debug(keyword + "=" + value);
+      final Pattern keywordPattern = Pattern.compile("\\$" + keyword + "\\$");
+      substitutedContent = keywordPattern.matcher(substitutedContent).replaceAll("\\$"
+          + keyword + ": " + value + " \\$");
+    }
+    return substitutedContent;
+  }
+
+}
