7380b0c6a95f51352f29e84e9d0f7cad6c7cd668
Jesper Hammarbäck <jesper@hammarback.eu>
2008-02-19 21:40:57 +0000

    [maven-release-plugin]  copy for tag sventon-1.4-rc1

---
+++ web/screenshots.php
@@ -0,0 +1,87 @@
+<div class="header">Screenshots</div>
+<div>
+  <b><i>Updated screenshots from sventon 1.4 comming soon!</i></b>
+</div>
+<div class="body">
+
+<hr>
+
+<p>
+  <b>Mac OS X screenshots</b>
+</p>
+<table style="border: 1px solid;">
+  <tr>
+    <th>Flattened directory</th>
+    <th>Thumbnail image view</th>
+  </tr>
+  <tr>
+    <td><a href="images/screenshots/osx_flatten.png"><img width="150" height="100" alt="Flattened directory" src="images/screenshots/osx_flatten.png"/></a></td>
+    <td><a href="images/screenshots/osx_thumbnail.png"><img width="150" height="100" alt="Thumbnail image view" src="images/screenshots/osx_thumbnail.png"/></a></td>
+  </tr>
+  <tr>
+    <th>Archive file view</th>
+    <th>File view</th>
+  </tr>
+  <tr>
+    <td><a href="images/screenshots/osx_archive.png"><img width="150" height="100" alt="Archive file view" src="images/screenshots/osx_archive.png"/></a></td>
+    <td><a href="images/screenshots/osx_showfile.png"><img width="150" height="100" alt="File view" src="images/screenshots/osx_showfile.png"/></a></td>
+  </tr>
+  <tr>
+    <th>File diff</th>
+    <th>Search logs</th>
+  </tr>
+  <tr>
+    <td><a href="images/screenshots/osx_diff.png"><img width="150" height="100" alt="File diff" src="images/screenshots/osx_diff.png"/></a></td>
+    <td><a href="images/screenshots/osx_logsearch.png"><img width="150" height="100" alt="Search logs" src="images/screenshots/osx_logsearch.png"/></a></td>
+  </tr>
+  <tr>
+    <th>Blame</th>
+    <th></th>
+  </tr>
+  <tr>
+    <td><a href="images/screenshots/osx_blame.png"><img width="150" height="100" alt="Blame" src="images/screenshots/osx_blame.png"/></a></td>
+    <td></td>
+  </tr>
+</table>
+
+<hr>
+
+<p>
+  <b>Win XP screenshots</b>
+</p>
+
+<table style="border: 1px solid;">
+  <tr>
+    <th>Flattened directory</th>
+    <th>Thumbnail image view</th>
+  </tr>
+  <tr>
+    <td><a href="images/screenshots/win_flatten.png"><img width="150" height="100" alt="Flattened directory" src="images/screenshots/win_flatten.png"/></a></td>
+    <td><a href="images/screenshots/win_thumbnail.png"><img width="150" height="100" alt="Thumbnail image view" src="images/screenshots/win_thumbnail.png"/></a></td>
+  </tr>
+  <tr>
+    <th>Archive file view</th>
+    <th>File view</th>
+  </tr>
+  <tr>
+    <td><a href="images/screenshots/win_archive.png"><img width="150" height="100" alt="Archive file view" src="images/screenshots/win_archive.png"/></a></td>
+    <td><a href="images/screenshots/win_showfile.png"><img width="150" height="100" alt="File view" src="images/screenshots/win_showfile.png"/></a></td>
+  </tr>
+  <tr>
+    <th>File diff</th>
+    <th>Search logs</th>
+  </tr>
+  <tr>
+    <td><a href="images/screenshots/win_diff.png"><img width="150" height="100" alt="File diff" src="images/screenshots/win_diff.png"/></a></td>
+    <td><a href="images/screenshots/win_logsearch.png"><img width="150" height="100" alt="Search logs" src="images/screenshots/win_logsearch.png"/></a></td>
+  </tr>
+  <tr>
+    <th>Blame</th>
+    <th></th>
+  </tr>
+  <tr>
+    <td><a href="images/screenshots/win_blame.png"><img width="150" height="100" alt="Blame" src="images/screenshots/win_blame.png"/></a></td>
+    <td></td>
+  </tr>
+</table>
+</div>
