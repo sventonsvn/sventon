aef562f837024b89ca3d2767edaa519a243a2b4a
Jesper Hammarbäck <jesper@hammarback.eu>
2008-03-14 20:23:47 +0000

    Merge of 1.4 branch, revisions 1239-1243.

--- sventon/web/downloads.php
+++ sventon/web/downloads.php
@@ -1,9 +1,9 @@
 <b>Available versions:</b><br/>
 
 <div style="padding-left:3px;padding-top:5px;">
-  <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v14rc2'); ">
-    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>1.4 RC2 (beta)</a>
-  <a href="/download/sventon-1.4-rc2-bin.zip" onClick="javascript:urchinTracker ('/downloads/v14rc2');">(mirror)</a>
+  <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v14rc3'); ">
+    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>1.4 RC3 (beta)</a>
+  <a href="/download/sventon-1.4-rc3-bin.zip" onClick="javascript:urchinTracker ('/downloads/v14rc3');">(mirror)</a>
   <br/>
   <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v130'); ">
     <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>1.3.0 (stable)</a>
