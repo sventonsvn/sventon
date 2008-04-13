b69d48fe033b50a230a0811ccd3308c6965d73ed
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2008-04-13 12:42:25 +0000

    Added more tests.

--- trunk/sventon/src/test/java/de/berlios/sventon/repository/cache/entrycache/EntryCacheTest.java
+++ trunk/sventon/src/test/java/de/berlios/sventon/repository/cache/entrycache/EntryCacheTest.java
@@ -1,13 +1,10 @@
 package de.berlios.sventon.repository.cache.entrycache;
 
+import de.berlios.sventon.TestUtils;
 import de.berlios.sventon.repository.RepositoryEntry;
 import static de.berlios.sventon.repository.RepositoryEntry.Kind.*;
 import junit.framework.TestCase;
-import org.tmatesoft.svn.core.SVNDirEntry;
-import org.tmatesoft.svn.core.SVNNodeKind;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -24,7 +21,7 @@ public class EntryCacheTest extends TestCase {
     final EntryCache cache = new MemoryCache();
 
     assertEquals(0, cache.getSize());
-    cache.add(getEntryTemplateList());
+    cache.add(TestUtils.getDirectoryList());
     assertEquals(13, cache.getSize());
     cache.clear();
     assertEquals(0, cache.getSize());
@@ -34,7 +31,7 @@ public class EntryCacheTest extends TestCase {
     final EntryCache cache = new MemoryCache();
 
     assertEquals(0, cache.getSize());
-    cache.add(getEntryTemplateList());
+    cache.add(TestUtils.getDirectoryList());
     assertEquals(13, cache.getSize());
   }
 
@@ -42,7 +39,7 @@ public class EntryCacheTest extends TestCase {
     final EntryCache cache = new MemoryCache();
 
     assertEquals(0, cache.getSize());
-    cache.add(getEntryTemplateList());
+    cache.add(TestUtils.getDirectoryList());
     assertEquals(13, cache.getSize());
 
     assertEquals(2, cache.findEntry("tag", "/tags/").size());
@@ -52,7 +49,7 @@ public class EntryCacheTest extends TestCase {
     final EntryCache cache = new MemoryCache();
 
     assertEquals(0, cache.getSize());
-    cache.add(getEntryTemplateList());
+    cache.add(TestUtils.getDirectoryList());
     assertEquals(13, cache.getSize());
 
     cache.removeByName("/file1.java", false);
@@ -79,7 +76,7 @@ public class EntryCacheTest extends TestCase {
     final EntryCache cache = new MemoryCache();
 
     assertEquals(0, cache.getSize());
-    cache.add(getEntryTemplateList());
+    cache.add(TestUtils.getDirectoryList());
     assertEquals(13, cache.getSize());
 
     assertEquals(5, cache.findByPattern(Pattern.compile(".*[12].*"), any).size());
@@ -94,27 +91,6 @@ public class EntryCacheTest extends TestCase {
     assertEquals(1, cache.findByPattern(Pattern.compile(".*/trunk/src/.*"), file).size());
 
     assertEquals(1, cache.findByPattern(Pattern.compile(".*/TrUnK/sRc/.*", Pattern.CASE_INSENSITIVE), file).size());
-  }
-
-  private List<RepositoryEntry> getEntryTemplateList() {
-    final List<RepositoryEntry> entries = new ArrayList<RepositoryEntry>();
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "trunk", SVNNodeKind.DIR, 0, false, 1, new Date(), "jesper"), "/"));
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "tags", SVNNodeKind.DIR, 0, false, 1, new Date(), "jesper"), "/"));
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "file1.java", SVNNodeKind.FILE, 64000, false, 1, new Date(), "jesper"), "/"));
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "file2.html", SVNNodeKind.FILE, 32000, false, 2, new Date(), "jesper"), "/"));
-
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "tagfile.txt", SVNNodeKind.FILE, 3200, false, 2, new Date(), "jesper"), "/tags/"));
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "tagfile2.txt", SVNNodeKind.FILE, 1600, false, 3, new Date(), "jesper"), "/tags/"));
-
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "code", SVNNodeKind.DIR, 0, false, 1, new Date(), "jesper"), "/trunk/"));
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "src", SVNNodeKind.DIR, 0, false, 1, new Date(), "jesper"), "/trunk/"));
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "DirFile3.java", SVNNodeKind.FILE, 1600, false, 3, new Date(), "jesper"), "/trunk/"));
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "file1_in_trunk.java", SVNNodeKind.FILE, 6400, false, 1, new Date(), "jesper"), "/trunk/"));
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "TestDirFile3.java", SVNNodeKind.FILE, 1600, false, 3, new Date(), "jesper"), "/trunk/"));
-
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "File3.java", SVNNodeKind.FILE, 16000, false, 3, new Date(), "jesper"), "/trunk/code/"));
-    entries.add(new RepositoryEntry(new SVNDirEntry(null, "DirFile2.html", SVNNodeKind.FILE, 3200, false, 2, new Date(), "jesper"), "/trunk/src/"));
-    return entries;
   }
 
   private void print(List<RepositoryEntry> entries) {
--- trunk/sventon/src/test/java/de/berlios/sventon/web/support/FileExtensionFilterTest.java
+++ trunk/sventon/src/test/java/de/berlios/sventon/web/support/FileExtensionFilterTest.java
@@ -1,21 +1,16 @@
 package de.berlios.sventon.web.support;
 
 import de.berlios.sventon.repository.RepositoryEntry;
+import de.berlios.sventon.TestUtils;
 import junit.framework.TestCase;
-import org.tmatesoft.svn.core.SVNDirEntry;
-import org.tmatesoft.svn.core.SVNNodeKind;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class FileExtensionFilterTest extends TestCase {
 
   public void testFilter() throws Exception {
 
-    final List<RepositoryEntry> list = new ArrayList<RepositoryEntry>();
-    list.add(new RepositoryEntry(new SVNDirEntry(null, "test.abC", SVNNodeKind.FILE, 0, false, 0, null, null), "/"));
-    list.add(new RepositoryEntry(new SVNDirEntry(null, "test.jpg", SVNNodeKind.FILE, 0, false, 0, null, null), "/"));
-    list.add(new RepositoryEntry(new SVNDirEntry(null, "test.GIF", SVNNodeKind.FILE, 0, false, 0, null, null), "/"));
+    final List<RepositoryEntry> list = TestUtils.getFileEntriesDirectoryList();
 
     List<RepositoryEntry> filteredList;
     filteredList = new FileExtensionFilter("jpg").filter(list);
