533c4913cdb3bd9a2fd7ca5af82d9d367501adde
Jesper Hammarbäck <jesper@hammarback.eu>
2008-06-14 18:50:31 +0000

    Deleted unused classes. Moved RSSController into ctrl package.

--- sventon/src/main/java/de/berlios/sventon/web/ctrl/RSSController.java
+++ sventon/src/main/java/de/berlios/sventon/web/ctrl/RSSController.java
@@ -9,31 +9,35 @@
  * newer version instead, at your option.
  * ====================================================================
  */
-package de.berlios.sventon.web.ctrl.xml;
+package de.berlios.sventon.web.ctrl;
 
 import de.berlios.sventon.appl.Application;
 import de.berlios.sventon.appl.RepositoryConfiguration;
 import de.berlios.sventon.appl.RepositoryName;
 import de.berlios.sventon.repository.RepositoryFactory;
+import de.berlios.sventon.rss.FeedGenerator;
 import de.berlios.sventon.service.RepositoryService;
-import de.berlios.sventon.web.model.UserRepositoryContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 import org.tmatesoft.svn.core.SVNAuthenticationException;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNLogEntry;
 import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.wc.SVNRevision;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
- * Controller used for generating latest commit info as an XML representation.
+ * Controller used for generating RSS feeds.
  *
  * @author jesper@users.berlios.de
  */
-public final class ShowLatestCommitInfoController extends AbstractController {
+public final class RSSController extends AbstractController {
 
   /**
    * Logger for this class and subclasses.
@@ -41,26 +45,26 @@ public final class ShowLatestCommitInfoController extends AbstractController {
   private final Log logger = LogFactory.getLog(getClass());
 
   /**
+   * RSS mime type, default set to <tt>application/xml; charset=UTF-8</tt>.
+   */
+  private String mimeType = "application/xml; charset=UTF-8";
+
+  /**
    * The application.
    */
   private Application application;
 
   /**
-   * The xml encoding.
+   * The feed generator.
    */
-  private String encoding;
-
-  /**
-   * Date pattern.
-   */
-  private String datePattern;
+  private FeedGenerator feedGenerator;
 
   /**
    * Service.
    */
   private RepositoryService repositoryService;
 
-  private static final String ERROR_MESSAGE = "Unable to get latest commit info";
+  private static final String ERROR_MESSAGE = "Unable to generate RSS feed";
 
   /**
    * The repository factory.
@@ -73,34 +77,49 @@ public final class ShowLatestCommitInfoController extends AbstractController {
   protected ModelAndView handleRequestInternal(final HttpServletRequest request, final HttpServletResponse response)
       throws Exception {
 
-    logger.debug("Getting latest commit info");
-    response.setContentType("text/xml");
+    logger.debug("Getting RSS feed");
+    response.setContentType(mimeType);
     response.setHeader("Cache-Control", "no-cache");
 
     final RepositoryName repositoryName = new RepositoryName(ServletRequestUtils.getRequiredStringParameter(request, "name"));
-    final UserRepositoryContext userRepositoryContext = UserRepositoryContext.getContext(request, repositoryName);
+    final String path = ServletRequestUtils.getStringParameter(request, "path", "/");
+    final String revision = ServletRequestUtils.getStringParameter(request, "revision", "HEAD");
+    final String uid = ServletRequestUtils.getStringParameter(request, "uid", null);
+    final String pwd = ServletRequestUtils.getStringParameter(request, "pwd", null);
+
+    if (!application.isConfigured()) {
+      String errorMessage = "sventon has not been configured yet!";
+      logger.error(errorMessage);
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, errorMessage);
+      return null;
+    }
+
     final RepositoryConfiguration configuration = application.getRepositoryConfiguration(repositoryName);
+    if (configuration == null) {
+      String errorMessage = "Repository [" + repositoryName + "] does not exist!";
+      logger.error(errorMessage);
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, errorMessage);
+      return null;
+    }
 
     SVNRepository repository = null;
     try {
       if (configuration.isAccessControlEnabled()) {
         repository = repositoryFactory.getRepository(configuration.getName(),
-            configuration.getSVNURL(), userRepositoryContext.getUid(), userRepositoryContext.getPwd());
+            configuration.getSVNURL(), uid, pwd);
       } else {
         repository = repositoryFactory.getRepository(configuration.getName(),
             configuration.getSVNURL(), configuration.getUid(), configuration.getPwd());
       }
 
-      final long headRevision = repositoryService.getLatestRevision(repository);
-      logger.debug("Latest revision is: " + headRevision);
-
-      response.getWriter().write(XMLDocumentHelper.getAsString(
-          XMLDocumentHelper.createXML(repositoryService.getRevision(
-              repositoryName, repository, headRevision), datePattern), encoding));
+      logger.debug("Outputting feed for [" + path + "]");
+      final List<SVNLogEntry> logEntries = repositoryService.getRevisions(
+          repositoryName, repository, SVNRevision.parse(revision).getNumber(), 1, path, configuration.getRssItemsCount());
+      feedGenerator.outputFeed(repositoryName, logEntries, request, response);
     } catch (SVNAuthenticationException ae) {
       logger.info(ERROR_MESSAGE + " - " + ae.getMessage());
       response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ae.getMessage());
-    } catch (Exception ex) {
+    } catch (SVNException ex) {
       logger.warn(ERROR_MESSAGE, ex);
       response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, ERROR_MESSAGE);
     } finally {
@@ -130,21 +149,21 @@ public final class ShowLatestCommitInfoController extends AbstractController {
   }
 
   /**
-   * Sets the xml encoding.
+   * Sets the mime-type for the feed.
    *
-   * @param encoding The encoding
+   * @param mimeType The mime-type
    */
-  public void setEncoding(final String encoding) {
-    this.encoding = encoding;
+  public void setMimeType(final String mimeType) {
+    this.mimeType = mimeType;
   }
 
   /**
-   * Sets the date pattern.
+   * Sets the feed generator.
    *
-   * @param datePattern The date pattern
+   * @param feedGenerator The generator.
    */
-  public void setDatePattern(final String datePattern) {
-    this.datePattern = datePattern;
+  public void setFeedGenerator(final FeedGenerator feedGenerator) {
+    this.feedGenerator = feedGenerator;
   }
 
   /**
