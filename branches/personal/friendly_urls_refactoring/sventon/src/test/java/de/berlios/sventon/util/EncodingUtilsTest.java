1e0bc359d1a5126b1989f324e392e2ea1e92eea0
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2008-07-07 17:22:04 +0000

    Fixed URL encoding error when doing redirects.

--- branches/personal/friendly_urls_refactoring/sventon/src/test/java/de/berlios/sventon/util/EncodingUtilsTest.java
+++ branches/personal/friendly_urls_refactoring/sventon/src/test/java/de/berlios/sventon/util/EncodingUtilsTest.java
@@ -8,4 +8,10 @@ public class EncodingUtilsTest extends TestCase {
     assertEquals("%2F", EncodingUtils.encode("/"));
     assertEquals("+", EncodingUtils.encode(" "));
   }
+
+  public void testEncodeUrl() throws Exception {
+    assertEquals("/%C3%BC.txt", EncodingUtils.encodeUrl("/ь.txt"));
+    assertEquals("/%C3%A5%C3%A4%C3%B6.txt", EncodingUtils.encodeUrl("/едц.txt"));
+  }
+
 }
