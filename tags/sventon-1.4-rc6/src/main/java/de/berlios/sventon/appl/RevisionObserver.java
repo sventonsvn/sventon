6855465ecc8f1285ecd3f47a5ccc3b0dc5ed7a5d
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2008-07-09 08:51:18 +0000

    [maven-release-plugin]  copy for tag sventon-1.4-rc6

---
+++ tags/sventon-1.4-rc6/src/main/java/de/berlios/sventon/appl/RevisionObserver.java
@@ -0,0 +1,31 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2005-2008 sventon project. All rights reserved.
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution. The terms
+ * are also available at http://www.sventon.org.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package de.berlios.sventon.appl;
+
+import java.util.Observer;
+
+/**
+ * RevisionObserver. Interface to be implemented by
+ * observers interested in getting published repository changes.
+ *
+ * @author jesper@user.berlios.de
+ */
+public interface RevisionObserver extends Observer {
+
+  /**
+   * Called to update the observer.
+   *
+   * @param revisionUpdate The updated revisions.
+   */
+  void update(final RevisionUpdate revisionUpdate);
+
+}
