b6ec026c3694bb1fa92ed2999a32c31a6dfc9845
Jesper Hammarbäck <jesper@hammarback.eu>
2008-07-25 19:39:58 +0000

    [maven-release-plugin]  copy for tag sventon-1.4.0

---
+++ src/main/java/de/berlios/sventon/logging/SVNLog4JAdapter.java
@@ -0,0 +1,73 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2005-2008 sventon project. All rights reserved.
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution. The terms
+ * are also available at http://www.sventon.org.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package de.berlios.sventon.logging;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.tmatesoft.svn.util.SVNDebugLogAdapter;
+
+/**
+ * Log4J sventon adapter for SVNKit logging.
+ *
+ * @author patrikfr@users.berlios.de
+ */
+public final class SVNLog4JAdapter extends SVNDebugLogAdapter {
+
+  /**
+   * The <tt>Log</tt> instance.
+   */
+  private final Log logger;
+
+  /**
+   * Constructor.
+   *
+   * @param namespace Logging name space to use.
+   */
+  public SVNLog4JAdapter(final String namespace) {
+    logger = LogFactory.getLog(namespace);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void log(final String message, final byte[] data) {
+    logger.debug(message + "\n" + new String(data));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void logInfo(final String message) {
+    logger.info(message);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void logError(final String message) {
+    logger.error(message);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void logInfo(final Throwable th) {
+    logger.info(th);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void logError(final Throwable th) {
+    logger.error(th);
+  }
+}
