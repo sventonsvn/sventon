53cf960d3b4ea0a37a4e96f97c28be2a6457149c
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2008-10-26 18:56:03 +0000

    Refactored export directory code.

---
+++ trunk/sventon/src/main/java/org/sventon/export/ExpirationRule.java
@@ -0,0 +1,31 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2005-2008 sventon project. All rights reserved.
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution. The terms
+ * are also available at http://www.sventon.org.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package org.sventon.export;
+
+import java.io.File;
+
+/**
+ * File expiration rule.
+ *
+ * @author jesper@sventon.org
+ */
+public interface ExpirationRule {
+
+  /**
+   * Returns true if given file has expired.
+   *
+   * @param file File
+   * @return True if file has expired according to the rules, false if not.
+   */
+  boolean hasExpired(final File file);
+
+}
