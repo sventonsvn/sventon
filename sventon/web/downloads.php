eebfba96d67590c7048a102018953b62c9de1170
Jesper Hammarbäck <jesper@hammarback.eu>
2009-02-22 20:07:28 +0000

    Prepared for release of version 2.0.4.

--- sventon/web/downloads.php
+++ sventon/web/downloads.php
@@ -2,9 +2,9 @@
 
 <div style="padding-left:3px;padding-top:5px;">
 
-  <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v203'); ">
-    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>2.0.3</a>
-  <a href="/download/sventon-2.0.3-bin.zip" onClick="javascript:urchinTracker ('/downloads/v203');">(mirror)</a>
+  <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v204'); ">
+    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>2.0.4</a>
+  <a href="/download/sventon-2.0.4-bin.zip" onClick="javascript:urchinTracker ('/downloads/v204');">(mirror)</a>
   <br/>
 
   <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v141'); ">
