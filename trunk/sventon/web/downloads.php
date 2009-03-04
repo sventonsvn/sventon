8544851c320b6259ad6190e263fe11a133025ee0
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2009-03-04 20:52:07 +0000

    Prepared for release of sventon 2.0.5.

--- trunk/sventon/web/downloads.php
+++ trunk/sventon/web/downloads.php
@@ -2,9 +2,9 @@
 
 <div style="padding-left:3px;padding-top:5px;">
 
-  <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v204'); ">
-    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>2.0.4</a>
-  <a href="/download/sventon-2.0.4-bin.zip" onClick="javascript:urchinTracker ('/downloads/v204');">(mirror)</a>
+  <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v205'); ">
+    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>2.0.5</a>
+  <a href="/download/sventon-2.0.5-bin.zip" onClick="javascript:urchinTracker ('/downloads/v205');">(mirror)</a>
   <br/>
 
   <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v141'); ">
