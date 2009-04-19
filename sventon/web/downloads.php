21ce4a2e858d27af1bf9db24c8ad2664cd73d142
Jesper Hammarbäck <jesper@hammarback.eu>
2009-04-19 12:58:16 +0000

    Removed mirror links for older versions.

--- sventon/web/downloads.php
+++ sventon/web/downloads.php
@@ -3,17 +3,19 @@
 <div style="padding-left:3px;padding-top:5px;">
 
   <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v210'); ">
-    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>&nbsp;2.1.0</a>
+    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>&nbsp;2.1.0
+  </a>
   <a href="/download/sventon-2.1.0-bin.zip" onClick="javascript:urchinTracker ('/downloads/v210');">(mirror)</a>
 
   <br/>
   <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v205'); ">
-    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>&nbsp;2.0.5</a>
-  <a href="/download/sventon-2.0.5-bin.zip" onClick="javascript:urchinTracker ('/downloads/v205');">(mirror)</a>
+    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>&nbsp;2.0.5
+  </a>
 
   <br/>
   <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v141'); ">
-    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>&nbsp;1.4.1</a>
-  <a href="/download/sventon-1.4.1-bin.zip" onClick="javascript:urchinTracker ('/downloads/v141');">(mirror)</a>
+    <img src="images/disk.png" align="absmiddle" vspace="2" hspace="6" border="0"/>&nbsp;1.4.1
+  </a>
+
   <br/>
 </div>
