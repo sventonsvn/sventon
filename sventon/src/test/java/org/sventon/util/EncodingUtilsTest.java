dd2b110cc4f692cd386778304742461a8f5dd208
Jesper Hammarbäck <jesper@hammarback.eu>
2009-08-19 19:09:52 +0000

    Added some tests.

--- sventon/src/test/java/org/sventon/util/EncodingUtilsTest.java
+++ sventon/src/test/java/org/sventon/util/EncodingUtilsTest.java
@@ -6,12 +6,25 @@ public class EncodingUtilsTest extends TestCase {
 
   public void testEncode() throws Exception {
     assertEquals("%2F", EncodingUtils.encode("/"));
+    assertEquals("%2B", EncodingUtils.encode("+"));
+    assertEquals("%23", EncodingUtils.encode("#"));
     assertEquals("+", EncodingUtils.encode(" "));
   }
 
   public void testEncodeUrl() throws Exception {
-    assertEquals("/%C3%BC.txt", EncodingUtils.encodeUrl("/\u00fc.txt"));
+    assertEquals("/test/%C3%BC.txt", EncodingUtils.encodeUrl("/test/\u00fc.txt"));
     assertEquals("/%C3%A5%C3%A4%C3%B6.txt", EncodingUtils.encodeUrl("/\u00e5\u00e4\u00f6.txt"));
+    assertEquals("/%2B/test.txt", EncodingUtils.encodeUrl("/+/test.txt"));
+    assertEquals("/%23/test.txt", EncodingUtils.encodeUrl("/#/test.txt"));
+    assertEquals("+", EncodingUtils.encodeUrl(" "));
+    assertEquals("", EncodingUtils.encodeUrl(""));
+
+    try {
+      EncodingUtils.encodeUrl(null);
+      fail("Should cause NPE");
+    } catch (Exception e) {
+      // expected
+    }
   }
 
 }
