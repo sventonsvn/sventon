74bb3bccc6b90bd4b545f3938dfb6180302fe09b
Jesper Hammarbäck <jesper@hammarback.eu>
2009-10-20 17:39:21 +0000

    Removed trilead and svnkit libraries, as we don't need to host these locally anymore.

--- sventon/lib/install.bat
+++ sventon/lib/install.bat
@@ -1,3 +1,1 @@
-call mvn install:install-file -Dfile=svnkit.jar -DpomFile=svnkit-pom.xml
-call mvn install:install-file -Dfile=trilead.jar -DpomFile=trilead-pom.xml
 call mvn install:install-file -Dfile=compass-2.2.0-M2.jar -DgroupId=org.compass-project -DartifactId=compass -Dversion=2.2.0-M2 -Dpackaging=jar
