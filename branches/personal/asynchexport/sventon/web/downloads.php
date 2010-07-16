a94b2269ada9e220a76f622ad2221d33b0b884a6
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2010-07-16 13:03:47 +0000

    Cleanup of old branches.

--- branches/personal/asynchexport/sventon/web/downloads.php
+++
@@ -1,3 +0,0 @@
-<p>
-Version 1.0.0 is now available for <a href="http://developer.berlios.de/project/showfiles.php?group_id=3670" onClick="javascript:urchinTracker ('/downloads/v100'); ">download</a>!
-</p>
