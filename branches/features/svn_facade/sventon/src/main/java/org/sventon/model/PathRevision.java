b7e20c3f1f9cc955fa268ea1f13e1b657e64f75d
jesper <jesper@bc96629c-2ea4-9108-d0e9-6d73f87fb883>
2010-08-10 22:14:33 +0000

    Made revision comparable. Added PathRevision to represent a path+revision combination.

---
+++ branches/features/svn_facade/sventon/src/main/java/org/sventon/model/PathRevision.java
@@ -0,0 +1,53 @@
+package org.sventon.model;
+
+/**
+ * Represents a, according to the repository, valid path/revision combination.
+ *
+ * @author jesper@sventon.org
+ */
+public class PathRevision implements Comparable<PathRevision> {
+
+  /**
+   * The delimiter between the path and the revision values.
+   */
+  public static final String DELIMITER = "@";
+
+  private final String path;
+
+  private final Revision revision;
+
+  /**
+   * Constructor.
+   *
+   * @param path     Path to the dir entry.
+   * @param revision Revision
+   */
+  public PathRevision(final String path, final Revision revision) {
+    this.path = path;
+    this.revision = revision;
+  }
+
+  /**
+   * @return The path
+   */
+  public String getPath() {
+    return path;
+  }
+
+  /**
+   * @return The revision
+   */
+  public Revision getRevision() {
+    return revision;
+  }
+
+  @Override
+  public int compareTo(PathRevision o) {
+    return this.getRevision().compareTo(o.getRevision());
+  }
+
+  @Override
+  public String toString() {
+    return path + DELIMITER + revision;
+  }
+}
