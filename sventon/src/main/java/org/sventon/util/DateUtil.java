36d53fa45f80832072d213ece55ace71689ab7b7
jorgenfalk <jorgenfalk@users.noreply.github.com>
2010-08-18 21:19:19 +0000

    DateUtil now parses a number of different date formats based around ISO8601, GNU and subverision log formats. Test checks all examples given in the svnbook.

--- sventon/src/main/java/org/sventon/util/DateUtil.java
+++ sventon/src/main/java/org/sventon/util/DateUtil.java
@@ -11,34 +11,201 @@
  */
 package org.sventon.util;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Misc helper for date operations such as uniform formatting etc
  */
 public class DateUtil {
   private static final String ISO8601_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'";
-  private static final String SHORT_FORMAT_PATTERN = "yyyy-MM-dd' 'HH:mm:ss'Z'";
 
-  // TODO: is this really ISO8601 or can we be more precise, like saying something like svn date format or...
+  /**
+   * Interface used by pattern matcher to calculate the time and date part
+   */
+  interface DateCalculator {
+    Calendar calculate(final Matcher matcher);
+  }
+
+  /**
+   *  Interface used by pattern matcher to adjust Time Zone to a given date (as Calendar)
+   */
+  interface ZoneClosure {
+    void execute(final Matcher matcher, Calendar calendar);
+  }
+
+  static final ZoneClosure basicZoneCalc = new ZoneClosure() {
+    @Override
+    public void execute(Matcher matcher, Calendar calendar) {
+      int zoneOffsetInMillis = "+".equals(matcher.group(10)) ? +1 : -1;
+      int hoursOffset = Integer.parseInt(matcher.group(11));
+      int minutesOffset = matcher.group(12) != null ? Integer.parseInt(matcher.group(13)) : 0;
+      zoneOffsetInMillis = zoneOffsetInMillis * ((hoursOffset*3600 + minutesOffset*60)*1000);
+
+      calendar.set(Calendar.ZONE_OFFSET, zoneOffsetInMillis);
+    }
+  };
+
+  static final ZoneClosure logZoneCalc = new ZoneClosure() {
+    @Override
+    public void execute(Matcher matcher, Calendar calendar) {
+      // Check if no TZD is given
+      if (matcher.group(10) == null){
+        return;
+      }
+
+      int zoneOffsetInMillis = "+".equals(matcher.group(11)) ? +1 : -1;
+      int hoursOffset = Integer.parseInt(matcher.group(12));
+      int minutesOffset = matcher.group(13) != null ? Integer.parseInt(matcher.group(13)) : 0;
+      zoneOffsetInMillis = zoneOffsetInMillis * ((hoursOffset*3600 + minutesOffset*60)*1000);
+
+      calendar.set(Calendar.ZONE_OFFSET, zoneOffsetInMillis);
+    }
+  };
+
+  static final ZoneClosure utcZoneCalc = new ZoneClosure() {
+    @Override
+    public void execute(Matcher matcher, Calendar calendar) {
+      if ("Z".equals(matcher.group(10))) {
+        calendar.set(Calendar.ZONE_OFFSET, 0);
+        calendar.set(Calendar.DST_OFFSET, 0);
+      }
+    }
+  };
+
+  static final ZoneClosure nullZoneCalc  = new ZoneClosure() {
+    @Override
+    public void execute(Matcher matcher, Calendar calendar) {
+      // Let it be.
+    }
+  };
+
+  static final DateCalculator basicDateCalc = new DateCalculator(){
+    @Override
+    public Calendar calculate(Matcher matcher) {
+      Calendar calendar = Calendar.getInstance();
+      calendar.clear();
+
+      int year = Integer.parseInt(matcher.group(1));
+      int month = Integer.parseInt(matcher.group(2));
+      int day = Integer.parseInt(matcher.group(3));
+
+      calendar.set(year, month - 1, day);
+
+      return calendar;
+    }
+  };
+
+  static final DateCalculator extendedDateCalc = new DateCalculator(){
+    @Override
+    public Calendar calculate(Matcher matcher) {
+      Calendar calendar = Calendar.getInstance();
+      calendar.clear();
+
+      int year = Integer.parseInt(matcher.group(1));
+      int month = Integer.parseInt(matcher.group(2));
+      int day = Integer.parseInt(matcher.group(3));
+      int hours = Integer.parseInt(matcher.group(4));
+      int minutes = Integer.parseInt(matcher.group(5));
+      int seconds = 0;
+      int milliseconds = 0;
+      if (matcher.group(6) != null) {
+        seconds = Integer.parseInt(matcher.group(7));
+        if (matcher.group(8) != null) {
+          String millis = matcher.group(9);
+          millis = millis.length() <= 3 ? millis : millis.substring(0, 3);
+          milliseconds = Integer.parseInt(millis);
+        }
+      }
+
+      calendar.set(year, month - 1, day);
+      calendar.set(Calendar.HOUR, hours);
+      calendar.set(Calendar.MINUTE, minutes);
+      calendar.set(Calendar.SECOND, seconds);
+      calendar.set(Calendar.MILLISECOND, milliseconds);
+
+      return calendar;
+    }
+  };
+
+  static final DateCalculator timeOnlyDateCalc = new DateCalculator() {
+    @Override
+    public Calendar calculate(Matcher matcher) {
+      Calendar calendar = Calendar.getInstance();
+      int hours = Integer.parseInt(matcher.group(1));
+      int minutes = Integer.parseInt(matcher.group(2));
+      int seconds = 0;
+      int milliseconds = 0;
+      if (matcher.group(3) != null) {
+        seconds = Integer.parseInt(matcher.group(4));
+        if (matcher.group(5) != null) {
+          String millis = matcher.group(6);
+          millis = millis.length() <= 3 ? millis : millis.substring(0, 3);
+          milliseconds = Integer.parseInt(millis);
+        }
+      }
+      calendar.set(Calendar.HOUR_OF_DAY, hours);
+      calendar.set(Calendar.MINUTE, minutes);
+      calendar.set(Calendar.SECOND, seconds);
+      calendar.set(Calendar.MILLISECOND, milliseconds);
+
+      return calendar;
+    }
+  };
+
+
+  /**
+   * DatePattern holds the pattern definition used to check for a match and if found, call the given DateCalculator and
+   * ZoneClosure to parse the date (as Calendar) for a revision string 
+   */
+  private enum DatePattern {
+    ISO8601_EXTENDED_DATE_ONLY_PATTERN("(\\d{4})-(\\d{1,2})-(\\d{1,2})", DateUtil.basicDateCalc, DateUtil.nullZoneCalc ),
+    ISO8601_EXTENDED_UTC_PATTERN("(\\d{4})-(\\d{1,2})-(\\d{1,2})T(\\d{1,2}):(\\d{2})(:(\\d{2})([.,](\\d{1,6}))?)?(Z)?", DateUtil.extendedDateCalc, DateUtil.utcZoneCalc),
+    ISO8601_EXTENDED_OFFSET_PATTERN("(\\d{4})-(\\d{1,2})-(\\d{1,2})T(\\d{1,2}):(\\d{2})(:(\\d{2})([.,](\\d{1,6}))?)?([+-])(\\d{2})(:(\\d{2}))?", DateUtil.extendedDateCalc, DateUtil.basicZoneCalc),
+    ISO8601_BASIC_DATE_ONLY_PATTERN("(\\d{4})(\\d{2})(\\d{2})", DateUtil.basicDateCalc, DateUtil.nullZoneCalc),
+    ISO8601_BASIC_UTC_PATTERN("(\\d{4})(\\d{2})(\\d{2})T(\\d{2})(\\d{2})((\\d{2})([.,](\\d{1,6}))?)?(Z)?", DateUtil.extendedDateCalc, DateUtil.utcZoneCalc),
+    ISO8601_BASIC_OFFSET_PATTERN("(\\d{4})(\\d{2})(\\d{2})T(\\d{2})(\\d{2})((\\d{2})([.,](\\d{1,6}))?)?([+-])(\\d{2})((\\d{2}))?", DateUtil.extendedDateCalc, DateUtil.basicZoneCalc),
+    ISO8601_GNU_FORMAT_PATTERN("(\\d{4})-(\\d{1,2})-(\\d{1,2})T(\\d{1,2}):(\\d{2})(:(\\d{2})([.,](\\d{1,6}))?)?([+-])(\\d{2})((\\d{2}))?", DateUtil.extendedDateCalc, DateUtil.basicZoneCalc),
+    SVN_LOG_DATE_FORMAT_PATTERN("(\\d{4})-(\\d{1,2})-(\\d{1,2}) (\\d{1,2}):(\\d{2})(:(\\d{2})([.,](\\d{1,6}))?)?( ([+-])(\\d{2})(\\d{2})?)?", DateUtil.extendedDateCalc, DateUtil.logZoneCalc),
+    TIME_ONLY_PATTERN("(\\d{1,2}):(\\d{2})(:(\\d{2})([.,](\\d{1,6}))?)?", DateUtil.timeOnlyDateCalc, DateUtil.nullZoneCalc);
+
+    private Pattern pattern;
+    private final DateCalculator dateCalculator;
+    private final ZoneClosure zoneClosure;
+
+    private DatePattern(final String datePattern, final DateUtil.DateCalculator dateCalc, final DateUtil.ZoneClosure zoneCalc ){
+      dateCalculator = dateCalc;
+      zoneClosure = zoneCalc;
+      this.pattern = Pattern.compile(datePattern);
+    }
+
+    public Calendar parse(final String date){
+      final Matcher matcher = pattern.matcher(date);
+      if (!matcher.matches()){
+        return null;
+      }
+
+      final Calendar calendar = dateCalculator.calculate(matcher);
+      zoneClosure.execute(matcher, calendar);
+      return calendar;
+    }
+
+  }
+
+
 
   public static Date parseISO8601(final String date) {
-    return parse(date, ISO8601_FORMAT_PATTERN);
-  }
-
-
-  public static Date parseShort(final String date) {
-    return parse(date, SHORT_FORMAT_PATTERN);
-  }
-
-  private static Date parse(String date, final String formatPattern) {
-    try {
-      return new SimpleDateFormat(formatPattern).parse(date);
-    } catch (ParseException e) {
-      throw new IllegalArgumentException(e);
+    for (DatePattern pattern : DatePattern.values()) {
+      final Calendar calendar = pattern.parse(date);
+      if (calendar != null){
+        return calendar.getTime();
+      }
     }
+
+    throw new IllegalArgumentException("Could not parse date " + date);
   }
 
 
@@ -46,10 +213,5 @@ public class DateUtil {
     if (date == null) return "";
     return new SimpleDateFormat(ISO8601_FORMAT_PATTERN).format(date);
   }
-
-  public static String formatShort(final Date date) {
-    return new SimpleDateFormat(SHORT_FORMAT_PATTERN).format(date);
-  }
-
 
 }
--- sventon/src/test/java/org/sventon/util/DateUtilTest.java
+++ sventon/src/test/java/org/sventon/util/DateUtilTest.java
@@ -16,21 +16,92 @@ import org.junit.Test;
 
 import java.util.Calendar;
 
-import static org.junit.Assert.fail;
+import static java.util.concurrent.TimeUnit.*;
 
 /**
  *
  */
 public class DateUtilTest {
-  @Test
-  public void parseISO8601() throws Exception {
-    //yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'
-    final Calendar cal = Calendar.getInstance();
-    cal.set(2010, 00, 01, 12, 34, 56);
-    cal.set(Calendar.MILLISECOND, 0);
 
-    Assert.assertEquals(cal.getTime(), DateUtil.parseISO8601("2010-01-01T12:34:56.000000Z"));
+  /*
+  Test ISO8601 + some SVN unique formats. svnbook gives the following examples:
+$ svn checkout -r {2006-02-17}
+$ svn checkout -r {15:30}
+$ svn checkout -r {15:30:00.200000}
+
+$ svn checkout -r {"2006-02-17 15:30"}
+$ svn checkout -r {"2006-02-17 15:30 +0230"}
+
+$ svn checkout -r {2006-02-17T15:30}
+$ svn checkout -r {2006-02-17T15:30Z}
+$ svn checkout -r {2006-02-17T15:30-04:00}
+
+$ svn checkout -r {20060217T1530}
+$ svn checkout -r {20060217T1530Z}
+$ svn checkout -r {20060217T1530-0500}
+   */
+  @Test
+  public void parseISO8601NoDate() throws Exception {
+    final Calendar today = Calendar.getInstance();
+    today.set(Calendar.HOUR_OF_DAY, 15);
+    today.set(Calendar.MINUTE, 30);
+    today.set(Calendar.SECOND, 0);
+    today.set(Calendar.MILLISECOND, 0);
+
+    Assert.assertEquals(today.getTime(), DateUtil.parseISO8601("15:30"));
+
+    // Note: We only use the last three digits (SSS) in the millisecond field, the rest is truncated.
+    today.set(Calendar.MILLISECOND, 200);
+    Assert.assertEquals(today.getTime(), DateUtil.parseISO8601("15:30:00.200000"));
   }
+  
+  @Test
+  public void parseISO8601Simple() throws Exception {
+    final Calendar cal = Calendar.getInstance();
+    cal.set(2006, 02 - 1, 17, 00, 00 ,00);
+    cal.set(Calendar.MILLISECOND, 0);
+    Assert.assertEquals(cal.getTime(), DateUtil.parseISO8601("2006-02-17"));
+
+    cal.set(Calendar.HOUR_OF_DAY, 15);
+    cal.set(Calendar.MINUTE, 30);
+    cal.set(Calendar.SECOND, 0);
+    Assert.assertEquals(cal.getTime(), DateUtil.parseISO8601("2006-02-17 15:30"));
+
+
+    cal.set(Calendar.ZONE_OFFSET, (int)(MILLISECONDS.convert(2, HOURS) + MILLISECONDS.convert(30, MINUTES)));
+    Assert.assertEquals(cal.getTime(), DateUtil.parseISO8601("2006-02-17 15:30 +0230"));
+  }
+
+  @Test
+  public void parseISO8601TFormat() throws Exception {
+    final Calendar cal = Calendar.getInstance();
+    cal.set(2006, 02 - 1, 17, 15, 30 ,00);
+    cal.set(Calendar.MILLISECOND, 0);
+    Assert.assertEquals(cal.getTime(), DateUtil.parseISO8601("2006-02-17T15:30"));
+
+    cal.set(Calendar.ZONE_OFFSET, 0);
+    Assert.assertEquals(cal.getTime(), DateUtil.parseISO8601("2006-02-17T15:30Z"));
+
+    cal.set(Calendar.ZONE_OFFSET, -(int)(MILLISECONDS.convert(4, HOURS)));
+    Assert.assertEquals(cal.getTime(), DateUtil.parseISO8601("2006-02-17T15:30-04:00"));
+  }
+
+  @Test
+  public void parseISO8601CompapctTFormat() throws Exception {
+    final Calendar cal = Calendar.getInstance();
+    cal.set(2006, 02 - 1, 17, 15, 30 ,00);
+    cal.set(Calendar.MILLISECOND, 0);
+    Assert.assertEquals(cal.getTime(), DateUtil.parseISO8601("20060217T1530"));
+
+    cal.set(Calendar.ZONE_OFFSET, 0);
+    Assert.assertEquals(cal.getTime(), DateUtil.parseISO8601("20060217T1530Z"));
+
+    cal.set(Calendar.ZONE_OFFSET, -(int)(MILLISECONDS.convert(4, HOURS)));
+    Assert.assertEquals(cal.getTime(), DateUtil.parseISO8601("20060217T1530-0400"));
+  }
+
+
+
   @Test
   public void formatISO8601() throws Exception {
     //yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'
@@ -41,14 +112,5 @@ public class DateUtilTest {
     Assert.assertEquals("2010-01-01T12:34:56.000000Z", DateUtil.formatISO8601(cal.getTime()));
   }
 
-  @Test
-  public void parseError() throws Exception {
-    try {
-      DateUtil.parseISO8601("2010-01-01 12:34:56");
-      fail("Parse error should throw IllegalArgumentException");
-    } catch (IllegalArgumentException e) {
-      // Expected
-    }
-  }
 
 }
