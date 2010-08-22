b51029ca5c48cfd607303773c7c251684152ba21
Jesper Hammarbäck <jesper@hammarback.eu>
2010-08-22 19:38:53 +0000

    Removed unnecessary compass-2.2.0-M2.jar.

--- sventon/lib/install.bat
+++ sventon/lib/install.bat
@@ -1,2 +1,1 @@
-call mvn install:install-file -Dfile=compass-2.2.0-M2.jar -DgroupId=org.compass-project -DartifactId=compass -Dversion=2.2.0-M2 -Dpackaging=jar
 call mvn install:install-file -Dfile=jhighlight-1.0-patched.jar -DgroupId=com.uwyn -DartifactId=jhighlight -Dversion=1.0-patched -Dpackaging=jar
