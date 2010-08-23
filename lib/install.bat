6d17d0db3793f8c2fa226b9bc9c59ee21ed09055
Jesper Hammarbäck <jesper@hammarback.eu>
2010-08-23 09:09:14 +0000

    Added svnjavahl.jar, version 1.6.12.

--- lib/install.bat
+++ lib/install.bat
@@ -1,1 +1,2 @@
 call mvn install:install-file -Dfile=jhighlight-1.0-patched.jar -DgroupId=com.uwyn -DartifactId=jhighlight -Dversion=1.0-patched -Dpackaging=jar
+call mvn install:install-file -Dfile=svnjavahl.jar -DgroupId=org.subversion -DartifactId=svnjavahl -Dversion=1.6.12 -Dpackaging=jar
