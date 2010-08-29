f6376d74ced65ed347501d7633cb91fef5d5cab0
Jesper Hammarbäck <jesper@hammarback.eu>
2010-08-29 19:10:30 +0000

    Fixed: Missing equals/hashCode in RevisionProperty made all calls to LogEntry's property map (<RevisionProperty, String>) return null.

--- sventon-core/src/main/java/org/sventon/model/RevisionProperty.java
+++ sventon-core/src/main/java/org/sventon/model/RevisionProperty.java
@@ -77,7 +77,21 @@ public class RevisionProperty implements Serializable {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof RevisionProperty)) return false;
+    RevisionProperty that = (RevisionProperty) o;
+    return !(name != null ? !name.equals(that.name) : that.name != null);
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
+
+  @Override
   public String toString() {
     return name;
   }
+
 }
