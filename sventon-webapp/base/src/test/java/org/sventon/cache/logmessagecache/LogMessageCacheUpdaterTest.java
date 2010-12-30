1ea9fe09560a71af0dc8d9551bd1835333958d24
Jesper Hammarbäck <jesper@hammarback.eu>
2010-12-30 00:49:05 +0000

    Added header comment to test source files.

--- sventon-webapp/base/src/test/java/org/sventon/cache/logmessagecache/LogMessageCacheUpdaterTest.java
+++ sventon-webapp/base/src/test/java/org/sventon/cache/logmessagecache/LogMessageCacheUpdaterTest.java
@@ -1,3 +1,14 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2005-2010 sventon project. All rights reserved.
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution. The terms
+ * are also available at http://www.sventon.org.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
 package org.sventon.cache.logmessagecache;
 
 import org.junit.Test;
