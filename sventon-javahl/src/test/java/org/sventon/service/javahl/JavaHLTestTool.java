b36d657ad705b073ae6e4d510bd2138f34871251
Jesper Hammarbäck <jesper@hammarback.eu>
2010-12-30 00:48:26 +0000

    Added header comment to test source files.

--- sventon-javahl/src/test/java/org/sventon/service/javahl/JavaHLTestTool.java
+++ sventon-javahl/src/test/java/org/sventon/service/javahl/JavaHLTestTool.java
@@ -1,3 +1,14 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2005-2010 sventon project. All rights reserved.
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution. The terms
+ * are also available at http://www.sventon.org.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
 package org.sventon.service.javahl;
 
 import org.sventon.SVNConnection;
@@ -43,7 +54,7 @@ public class JavaHLTestTool {
       final RepositoryService service = new JavaHLRepositoryService();
       final SVNClient client = new SVNClient();
       final SVNURL svnUrl = SVNURL.parse(url);
-      final SVNConnection connection = new JavaHLConnection(client, svnUrl, null);
+      final SVNConnection connection = new JavaHLConnection(client, svnUrl);
 
 
       System.out.println(client.getVersion());
