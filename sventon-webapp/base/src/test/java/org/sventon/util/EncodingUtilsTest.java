53f13e2b3c010d1ca19f4e31b6eb860530cbb6eb
Jesper Hammarbäck <jesper@hammarback.eu>
2011-01-03 20:59:39 +0000

    Encode space to "%20" instead of "+" in URL.

--- sventon-webapp/base/src/test/java/org/sventon/util/EncodingUtilsTest.java
+++ sventon-webapp/base/src/test/java/org/sventon/util/EncodingUtilsTest.java
@@ -32,7 +32,7 @@ public class EncodingUtilsTest {
     assertEquals("/%C3%A5%C3%A4%C3%B6.txt", EncodingUtils.encodeUrl("/\u00e5\u00e4\u00f6.txt"));
     assertEquals("/%2B/test.txt", EncodingUtils.encodeUrl("/+/test.txt"));
     assertEquals("/%23/test.txt", EncodingUtils.encodeUrl("/#/test.txt"));
-    assertEquals("+", EncodingUtils.encodeUrl(" "));
+    assertEquals("%20", EncodingUtils.encodeUrl(" "));
     assertEquals("", EncodingUtils.encodeUrl(""));
 
     try {
